17506f84636e79be306e7191b88b4a3bb7e40e2c
Kai Wolf <kai.wolf@gmail.com>
2019-10-11 11:52:12 +0200

    Add Conan dependencies for simpleini, tclap and asio

--- cmake/Modules/Findasio.cmake
+++ cmake/Modules/Findasio.cmake
@@ -1,28 +1,22 @@
 find_path(Asio_INCLUDE_DIR
   NAMES asio.hpp
-  PATHS
-  ${CMAKE_SOURCE_DIR}/thirdparty/asio/asio/include
-  include
-)
+  HINTS include ${CONAN_ASIO_ROOT}/include)
 
 if(Asio_INCLUDE_DIR-NOTFOUND)
   message(FATAL_ERROR "Could not find Asio library")
-  set(Asio_FOUND False)
+  set(Asio_FOUND FALSE)
 else()
-  set(Asio_FOUND True)
-  # Add workaround for stupid eprosima_find_package()
+  set(Asio_FOUND TRUE)
   set(ASIO_INCLUDE_DIR ${Asio_INCLUDE_DIR})
 endif()
 
 if(Asio_FOUND)
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Asio
-    REQUIRED_VARS Asio_INCLUDE_DIR
-  )
+    REQUIRED_VARS Asio_INCLUDE_DIR)
 
   if(NOT TARGET asio::asio)
     set(Asio_INCLUDE_DIRS ${Asio_INCLUDE_DIR})
-
     add_library(asio::asio INTERFACE IMPORTED)
     set_target_properties(asio::asio PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES ${Asio_INCLUDE_DIR}
--- cmake/Modules/Findtclap.cmake
+++ cmake/Modules/Findtclap.cmake
@@ -1,26 +1,21 @@
 find_path(tclap_INCLUDE_DIR
   NAMES tclap/Arg.h
-  PATHS 
-  include
-  ${CMAKE_SOURCE_DIR}/thirdparty/tclap/include
-)
+  HINTS include ${CONAN_TCLAP_ROOT}/include)
 
 if(tclap_INCLUDE_DIR-NOTFOUND)
   message(FATAL_ERROR "Could not find tclap library")
-  set(tclap_FOUND False)
+  set(tclap_FOUND FALSE)
 else()
-  set(tclap_FOUND True)
+  set(tclap_FOUND TRUE)
 endif()
 
 if(tclap_FOUND)
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(tclap
-    REQUIRED_VARS tclap_INCLUDE_DIR
-  )
+    REQUIRED_VARS tclap_INCLUDE_DIR)
 
   if(NOT TARGET tclap::tclap)
     set(tclap_INCLUDE_DIRS ${tclap_INCLUDE_DIR})
-
     add_library(tclap::tclap INTERFACE IMPORTED)
     set_target_properties(tclap::tclap PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES ${tclap_INCLUDE_DIR})
