d26eacc04ad10ed8465108a2cb0e04855288ca86
Kai Wolf <kai.wolf@gmail.com>
2019-11-07 15:17:06 +0100

    Add Conan integration (#18)
    * Add Conan dependencies for simpleini, tclap and asio
    
    * Add Conan protobuf package from bintray
    
    * Prepare CI build scripts for using Conan
    
    * Add spdlog from bincrafters
    
    * Add HDF5 package to conanfile.txt
    
    * Add own Qt5 Conan package (needed for WinExtras)
    
    * Add proto compiler as another dependency

--- cmake/Modules/Findsimpleini.cmake
+++ cmake/Modules/Findsimpleini.cmake
@@ -1,26 +1,25 @@
 find_path(simpleini_INCLUDE_DIR
   NAMES SimpleIni.h
-  PATHS 
-  include
-  ${CMAKE_SOURCE_DIR}/thirdparty/simpleini
-)
+  HINTS
+    include
+    ${CONAN_SIMPLEINI_ROOT}/include
+    ${CMAKE_SOURCE_DIR}/thirdparty/simpleini
+  )
 
 if(simpleini_INCLUDE_DIR-NOTFOUND)
   message(FATAL_ERROR "Could not find simpleini library")
-  set(simpleini_FOUND False)
+  set(simpleini_FOUND FALSE)
 else()
-  set(simpleini_FOUND True)
+    set(simpleini_FOUND TRUE)
 endif()
 
 if(simpleini_FOUND)
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(simpleini
-    REQUIRED_VARS simpleini_INCLUDE_DIR
-  )
+    REQUIRED_VARS simpleini_INCLUDE_DIR)
 
   if(NOT TARGET simpleini::simpleini)
     set(simpleini_INCLUDE_DIRS ${simpleini_INCLUDE_DIR})
-
     add_library(simpleini::simpleini INTERFACE IMPORTED)
     set_target_properties(simpleini::simpleini PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES ${simpleini_INCLUDE_DIR})
