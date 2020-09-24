e9b6fd05fd3b678cb190bbd13ce5f212899002ad
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2020-09-24 08:40:32 +0200

    The Documentation is now included in the windows setup

--- build_win/win_make_cmake.bat
+++ build_win/win_make_cmake.bat
@@ -1,6 +1,6 @@
 @echo off
 
-pushd %~dp0\..
+pushd "%~dp0\.."
 
 call build_win\win_set_vars.bat
 
@@ -24,11 +24,23 @@ if %VERSION% == v142 goto build
 
 :build
 
-set "CMAKE_OPTIONS_COMPLETE=-DCMAKE_INSTALL_PREFIX=_install -DBUILD_SHARED_LIBS=OFF"
+set "CMAKE_OPTIONS_COMPLETE=-DCMAKE_INSTALL_PREFIX=_install -DBUILD_SHARED_LIBS=OFF -DBUILD_DOCS=ON"
 set "CMAKE_OPTIONS_SDK=-DCMAKE_INSTALL_PREFIX=_install -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOCS=OFF -DBUILD_APPS=OFF -DBUILD_SAMPLES=OFF -DBUILD_TIME=ON -DBUILD_PY_BINDING=OFF -DBUILD_CSHARP_BINDING=OFF -DBUILD_ECAL_TESTS=OFF -DECAL_LAYER_ICEORYX=OFF -DECAL_INCLUDE_PY_SAMPLES=OFF -DECAL_INSTALL_SAMPLE_SOURCES=OFF -DCPACK_PACK_WITH_INNOSETUP=OFF"
 
-if not exist "%BUILD_DIR_COMPLETE%" mkdir %BUILD_DIR_COMPLETE%
-if not exist "%BUILD_DIR_SDK%" mkdir %BUILD_DIR_SDK%
+if not exist "%BUILD_DIR_COMPLETE%" mkdir "%BUILD_DIR_COMPLETE%"
+if not exist "%BUILD_DIR_SDK%" mkdir "%BUILD_DIR_SDK%"
+
+echo Creating Python venv
+if not exist "%BUILD_DIR_COMPLETE%\.venv" mkdir "%BUILD_DIR_COMPLETE%\.venv"
+python -m venv "%BUILD_DIR_COMPLETE%\.venv"
+CALL "%BUILD_DIR_COMPLETE%\.venv\Scripts\activate.bat"
+
+echo Upgrading pip
+python -m pip install --upgrade pip
+
+echo Installing python requirements
+pip install wheel
+pip install -r requirements.txt
 
 if %ARCH% == "" goto build_no_arch
 
