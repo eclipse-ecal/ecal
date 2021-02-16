d183e4a10d211f4a1d9e16ccd7b0771e93bd7489
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2021-02-16 16:03:28 +0100

    eCAL Player Slider can be dragged again

--- app/play/play_gui/src/widgets/player_controls_widget/player_time_slider.h
+++ app/play/play_gui/src/widgets/player_controls_widget/player_time_slider.h
@@ -1,15 +1,25 @@
 #pragma once
 
 #include <QSlider>
+#include <QPoint>
 
+/**
+ * QSlider that immediatelly jumps when you click on it.
+ */
 class PlayerTimeSlider : public QSlider
 {
   Q_OBJECT
 
+////////////////////////////////////
+// Constructor
+////////////////////////////////////
 public:
   PlayerTimeSlider(Qt::Orientation orientation, QWidget* parent = nullptr);
   PlayerTimeSlider(QWidget* parent = nullptr);
 
+////////////////////////////////////
+// Change Value by clicking
+////////////////////////////////////
 protected:
   void mousePressEvent(QMouseEvent* ev) override;
 };
