6e874892820940a8468071281607eaf6a2ae756d
Rex Schilasky <49162693+rex-schilasky@users.noreply.github.com>
2021-03-04 22:43:28 +0100

    Update win_make_cmake.bat
    X64 option only #202

--- build_win/win_make_cmake.bat
+++ build_win/win_make_cmake.bat
@@ -26,9 +26,9 @@ pip install wheel
 pip install -r requirements.txt
 
 cd /d "%WORKSPACE%\%BUILD_DIR_COMPLETE%"
-cmake ../.. %CMAKE_OPTIONS_COMPLETE%
+cmake ../.. -A x64 %CMAKE_OPTIONS_COMPLETE%
 
 cd /d "%WORKSPACE%\%BUILD_DIR_SDK%"
-cmake ../.. %CMAKE_OPTIONS_SDK%
+cmake ../.. -A x64 %CMAKE_OPTIONS_SDK%
 
 popd
