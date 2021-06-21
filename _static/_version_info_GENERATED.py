bf792c68f0242197d2c72b04b9abe6a0cec6aedf
FlorianReimold <FlorianReimold@users.noreply.github.com>
2021-06-21 10:43:46 +0000

    deploy: d89614e56124f6b5ad19b1d3da889389efdfee33

--- _static/_version_info_GENERATED.py
+++ _static/_version_info_GENERATED.py
@@ -1,10 +1,10 @@
 version_info = (
 {
-  "build_mtime": "1611167919",
+  "build_mtime": "1616763510",
   "module_name": "sphinx_typo3_theme",
-  "version_scm": "4.4.3",
+  "version_scm": "4.5.2",
   "version_scm_build": "",
-  "version_scm_core": "4.4.3",
+  "version_scm_core": "4.5.2",
   "version_scm_pre_release": ""
 }
 )
