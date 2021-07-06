0b9390411dc6ea085c8016641d11043a76853e36
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2021-07-06 13:03:10 +0200

    Updated npcap SDK and Pcap++

--- build_win/download_npcap.ps1
+++ build_win/download_npcap.ps1
@@ -4,8 +4,8 @@ New-Item "$target_dir" -ItemType "directory" -Force
 
 $webclient = New-Object System.Net.WebClient
 
-$webclient.DownloadFile("https://nmap.org/npcap/dist/npcap-sdk-1.05.zip","$target_dir\npcap-sdk-1.06.zip")
-$webclient.DownloadFile("https://github.com/seladb/PcapPlusPlus/releases/download/v20.08/pcapplusplus-20.08-windows-vs2015.zip","$target_dir\pcapplusplus-20.08-windows-vs2015.zip")
+$webclient.DownloadFile("https://nmap.org/npcap/dist/npcap-sdk-1.10.zip","$target_dir\npcap-sdk-1.10.zip")
+$webclient.DownloadFile("https://github.com/seladb/PcapPlusPlus/releases/download/v21.05/pcapplusplus-21.05-windows-vs2015.zip","$target_dir\pcapplusplus-21.05-windows-vs2015.zip")
 
-Expand-Archive -Force -Path "$target_dir\npcap-sdk-1.06.zip" -DestinationPath "$target_dir\npcap-sdk"
-Expand-Archive -Force -Path "$target_dir\pcapplusplus-20.08-windows-vs2015.zip" -DestinationPath "$target_dir"
+Expand-Archive -Force -Path "$target_dir\npcap-sdk-1.10.zip" -DestinationPath "$target_dir\npcap-sdk"
+Expand-Archive -Force -Path "$target_dir\pcapplusplus-21.05-windows-vs2015.zip" -DestinationPath "$target_dir"
