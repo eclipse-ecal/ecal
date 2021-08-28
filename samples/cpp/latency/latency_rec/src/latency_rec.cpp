e6665186a4cf14c7f9031f9b5b1943ab3e751769
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2021-08-28 11:13:31 +0200

    latency snd/rec samples with logging and callback delay

--- samples/cpp/latency/latency_rec/src/latency_rec.cpp
+++ samples/cpp/latency/latency_rec/src/latency_rec.cpp
@@ -19,11 +19,23 @@
 
 #include <ecal/ecal.h>
 
+#include <algorithm>
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <mutex>
 #include <numeric>
 #include <vector>
+
+#include <tclap/CmdLine.h>
+
+// warmup runs not to measure
+const int warmups(10);
+
+// helper
+long long get_microseconds();
+void evaluate(std::vector<long long>& lat_arr_, size_t rec_size_, size_t warmups_, std::string& log_file_);
+void log2file(std::vector<long long>& lat_arr_, size_t rec_size_, std::string& log_file_);
 
 // data structure for later evaluation
 struct SCallbackPar
@@ -35,15 +47,8 @@ struct SCallbackPar
   size_t                 msg_num  = 0;
 };
 
-// time getter
-long long get_microseconds()
-{
-  std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-  return(std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count());
-}
-
 // message receive callback
-void on_receive(const struct eCAL::SReceiveCallbackData* data_, SCallbackPar* par_)
+void on_receive(const struct eCAL::SReceiveCallbackData* data_, SCallbackPar* par_, int delay_, std::string& log_file_)
 {
   // get receive time stamp
   auto rec_time = get_microseconds();
@@ -53,17 +58,22 @@ void on_receive(const struct eCAL::SReceiveCallbackData* data_, SCallbackPar* pa
   par_->latency_array.push_back(rec_time - data_->time);
   par_->rec_size = data_->size;
   par_->msg_num++;
+
+  if(delay_ > 0) eCAL::Process::SleepMS(delay_);
 }
 
 // single test run
-void do_run()
+void do_run(int delay_, std::string& log_file_)
 {
+  // initialize eCAL API
+  eCAL::Initialize(0, nullptr, "latency_rec");
+
   // subscriber
   eCAL::CSubscriber sub("ping");
 
   // apply subscriber callback function
   SCallbackPar cb_par;
-  auto callback = std::bind(on_receive, std::placeholders::_2, &cb_par);
+  auto callback = std::bind(on_receive, std::placeholders::_2, &cb_par, delay_, log_file_);
   sub.AddReceiveCallback(callback);
 
   size_t msg_last(0);
@@ -83,35 +93,97 @@ void do_run()
   sub.RemReceiveCallback();
 
   // evaluate all
-  long long sum_msg = cb_par.latency_array.size();
-  std::cout << "--------------------------------------------" << std::endl;
-  std::cout << "Messages received             : " << sum_msg  << std::endl;
-  if (sum_msg > 0)
-  {
-    long long sum_time = std::accumulate(cb_par.latency_array.begin(), cb_par.latency_array.end(), 0LL);
-    long long avg_time = sum_time / sum_msg;
-    std::cout << "Message size received         : " << cb_par.rec_size/1024 << " kB" << std::endl;
-    std::cout << "Message average latency       : " << avg_time             << " us" << std::endl;
-    std::cout << "Throughput                    : " << static_cast<int>(((cb_par.rec_size * sum_msg) / 1024.0) / (sum_time / 1000.0 / 1000.0))          << " kB/s"  << std::endl;
-    std::cout << "                              : " << static_cast<int>(((cb_par.rec_size * sum_msg) / 1024.0 / 1024.0) / (sum_time / 1000.0 / 1000.0)) << " MB/s"  << std::endl;
-    std::cout << "                              : " << static_cast<int>(sum_msg / (sum_time / 1000.0 / 1000.0))                                         << " Msg/s" << std::endl;
-  }
-  std::cout << "--------------------------------------------" << std::endl;
-}
+  evaluate(cb_par.latency_array, cb_par.rec_size, warmups, log_file_);
 
-int main(int /*argc*/, char** /*argv*/)
-{
-  // initialize eCAL API
-  eCAL::Initialize(0, nullptr, "latency_rec");
-
-  while (eCAL::Ok())
-  {
-    // run test
-    do_run();
-  }
+  // log all latencies into file
+  log2file(cb_par.latency_array, cb_par.rec_size, log_file_);
 
   // finalize eCAL API
   eCAL::Finalize();
+}
+
+int main(int argc, char** argv)
+{
+  try
+  {
+    // parse command line
+    TCLAP::CmdLine cmd("latency_rec");
+    TCLAP::ValueArg<int>         delay(   "d", "delay",    "Messages send delay in ms.",    false,  0, "int");
+    TCLAP::ValueArg<std::string> log_file("l", "log_file", "File to export latency array.", false, "", "string");
+    cmd.add(delay);
+    cmd.add(log_file);
+    cmd.parse(argc, argv);
+
+    // run tests
+    do { do_run(delay.getValue(), log_file.getValue()); } while (eCAL::Ok());
+  }
+  catch (TCLAP::ArgException& e)  // catch any exceptions
+  {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+    return EXIT_FAILURE;
+  }
 
   return(0);
+}
+
+// time getter
+long long get_microseconds()
+{
+  std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+  return(std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count());
+}
+
+// evaluation
+void evaluate(std::vector<long long>& lat_arr_, size_t rec_size_, size_t warmups_, std::string& log_file_)
+{
+  std::stringstream ss;
+
+  // remove warmup runs
+  if (lat_arr_.size() >= warmups_)
+  {
+    lat_arr_.erase(lat_arr_.begin(), lat_arr_.begin() + warmups_);
+  }
+
+  // evaluate all
+  size_t sum_msg = lat_arr_.size();
+  ss << "--------------------------------------------" << std::endl;
+  ss << "Messages received             : " << sum_msg  << std::endl;
+  if (sum_msg > warmups_)
+  {
+    long long sum_time = std::accumulate(lat_arr_.begin(), lat_arr_.end(), 0LL);
+    long long avg_time = sum_time / sum_msg;
+    auto      min_it = std::min_element(lat_arr_.begin(), lat_arr_.end());
+    auto      max_it = std::max_element(lat_arr_.begin(), lat_arr_.end());
+    size_t    min_pos = min_it - lat_arr_.begin();
+    size_t    max_pos = max_it - lat_arr_.begin();
+    long long min_time = *min_it;
+    long long max_time = *max_it;
+    ss << "Message size received         : " << rec_size_ / 1024 << " kB"       << std::endl;
+    ss << "Message average latency       : " << avg_time << " us"               << std::endl;
+    ss << "Message min latency           : " << min_time << " us @ " << min_pos << std::endl;
+    ss << "Message max latency           : " << max_time << " us @ " << max_pos << std::endl;
+    ss << "Throughput                    : " << static_cast<int>(((rec_size_ * sum_msg) / 1024.0) / (sum_time / 1000.0 / 1000.0))          << " kB/s"  << std::endl;
+    ss << "                              : " << static_cast<int>(((rec_size_ * sum_msg) / 1024.0 / 1024.0) / (sum_time / 1000.0 / 1000.0)) << " MB/s"  << std::endl;
+    ss << "                              : " << static_cast<int>(sum_msg / (sum_time / 1000.0 / 1000.0))                                   << " Msg/s" << std::endl;
+  }
+  ss << "--------------------------------------------" << std::endl;
+
+  // log to console
+  std::cout << ss.str();
+
+  // log into logfile (append)
+  std::ofstream ofile;
+  ofile.open(log_file_, std::ios::out | std::ios::app);
+  ofile << ss.str();
+}
+
+void log2file(std::vector<long long>& lat_arr_, size_t rec_size_, std::string& log_file_)
+{
+  std::stringstream ss;
+  ss << std::setw(6) << std::setfill('0') << rec_size_/1024;
+  std::string rec_size_s = ss.str();
+
+  std::ofstream ofile(rec_size_s + "-" +  log_file_);
+  std::ostream_iterator<long long>output_iterator(ofile, "\n");
+  std::copy(lat_arr_.begin(), lat_arr_.end(), output_iterator);
 }
--- samples/cpp/latency/latency_snd/src/latency_snd.cpp
+++ samples/cpp/latency/latency_snd/src/latency_snd.cpp
@@ -19,8 +19,13 @@
 
 #include <ecal/ecal.h>
 
-#include <tclap/CmdLine.h>
+#include <chrono>
 #include <iostream>
+
+#include <tclap/CmdLine.h>
+
+// warmup runs not to measure
+const int warmups(10);
 
 // time getter
 long long get_microseconds()
@@ -30,13 +35,14 @@ long long get_microseconds()
 }
 
 // single test run
-void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, bool zero_copy)
+void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, int mem_buffer, bool zero_copy)
 {
   // log parameter
   std::cout << "--------------------------------------------"    << std::endl;
   std::cout << "Runs                    : " << runs              << std::endl;
   std::cout << "Message size            : " << snd_size << " kB" << std::endl;
   std::cout << "Message delay           : " << delay    << " ms" << std::endl;
+  std::cout << "Memory buffer           : " << mem_buffer        << std::endl;
   if (zero_copy)
   {
     std::cout << "Zero copy               : ON"  << std::endl;
@@ -52,6 +58,9 @@ void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, bool zero_cop
   // create publisher and subscriber
   eCAL::CPublisher pub("ping");
 
+  // set number of publisher memory buffers
+  pub.SetBufferCount(mem_buffer);
+
   // enable zero copy mode
   pub.EnableZeroCopy(zero_copy);
 
@@ -61,8 +70,9 @@ void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, bool zero_cop
   // let them match
   eCAL::Process::SleepMS(2000);
 
+  // add some extra loops for warmup :-)
   int run(0);
-  for (run = 0; run < runs; ++run)
+  for (run = 0; run < runs+warmups; ++run)
   {
     // get time and send message
     pub.Send(snd_array.data(), snd_array.size(), get_microseconds());
@@ -71,8 +81,11 @@ void do_run(const int runs, int snd_size /*kB*/, int delay /*ms*/, bool zero_cop
   }
 
   // log test
-  std::cout << "Messages sent           : " << run               << std::endl;
-  std::cout << "--------------------------------------------"    << std::endl;
+  std::cout << "Messages sent           : " << run - warmups << std::endl;
+  std::cout << "--------------------------------------------" << std::endl;
+
+  // let the receiver do the evaluation
+  eCAL::Process::SleepMS(2000);
 
   // finalize eCAL API
   eCAL::Finalize();
@@ -84,25 +97,27 @@ int main(int argc, char **argv)
   {
     // parse command line
     TCLAP::CmdLine cmd("latency_snd");
-    TCLAP::ValueArg<int> runs     ("r", "runs",      "Number of messages to send.", false, 1000, "int");
-    TCLAP::ValueArg<int> size     ("s", "size",      "Messages size in kB.",        false, 64,   "int");
-    TCLAP::ValueArg<int> delay    ("d", "delay",     "Messages send delay in ms.",  false, 10,   "int");
-    TCLAP::SwitchArg     zero_copy("z", "zero_copy", "Switch zero copy mode on.");
+    TCLAP::ValueArg<int>         runs      ("r", "runs",       "Number of messages to send.",            false, 1000, "int");
+    TCLAP::ValueArg<int>         size      ("s", "size",       "Messages size in kB.",                   false,   -1, "int");
+    TCLAP::ValueArg<int>         delay     ("d", "delay",      "Messages send delay in ms.",             false,   50, "int");
+    TCLAP::ValueArg<int>         mem_buffer("b", "mem_buffer", "Number of memory files per connection.", false,    1, "int");
+    TCLAP::SwitchArg             zero_copy ("z", "zero_copy",  "Switch zero copy mode on.");
     cmd.add(runs);
     cmd.add(size);
     cmd.add(delay);
+    cmd.add(mem_buffer);
     cmd.add(zero_copy);
     cmd.parse(argc, argv);
 
     if(size < 0)
     {
       // automatic size mode
-      for (int s = 1; s <= 16384; s *= 2) do_run(runs.getValue(), s, delay.getValue(), zero_copy.getValue());
+      for (int s = 1; s <= 16384; s *= 2) do_run(runs.getValue(), s, delay.getValue(), mem_buffer.getValue(), zero_copy.getValue());
     }
     else
     {
       // run single test
-      do_run(runs.getValue(), size.getValue(), delay.getValue(), zero_copy.getValue());
+      do_run(runs.getValue(), size.getValue(), delay.getValue(), mem_buffer.getValue(), zero_copy.getValue());
     }
   }
   catch (TCLAP::ArgException &e)  // catch any exceptions
