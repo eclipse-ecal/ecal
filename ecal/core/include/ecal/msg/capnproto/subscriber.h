f9abcc11bf5dcc96738aa58086fa09a1ecd5bc6a
Rex Schilasky <49162693+rex-schilasky@users.noreply.github.com>
2022-01-12 11:27:57 +0100

    wrong replacement in capnp subscriber

--- ecal/core/include/ecal/msg/capnproto/subscriber.h
+++ ecal/core/include/ecal/msg/capnproto/subscriber.h
@@ -240,7 +240,7 @@ namespace eCAL
       **/
       bool Receive(long long* time_ = nullptr, int rcv_timeout_ = 0)
       {
-        bool success = subscriber.ReceiveBuffer(builder, time_, rcv_timeout_);
+        bool success = subscriber.Receive(builder, time_, rcv_timeout_);
         // Update the Reader
         root_builder = typename message_type::Builder(builder.getRoot<message_type>());
         return success;
