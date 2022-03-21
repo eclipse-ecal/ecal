da03aafa651ba8d36a6743f779ec5c20202c6f06
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2022-03-21 15:40:10 +0100

    Added get Config functions for almost all settings that are inside ecal.ini

---
+++ ecal/core/src/ecal_config.cpp
@@ -0,0 +1,110 @@
+/* ========================= eCAL LICENSE =================================
+ *
+ * Copyright (C) 2016 - 2019 Continental Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================= eCAL LICENSE =================================
+*/
+
+#include <ecal/ecal_config.h>
+
+#include "ecal_config_reader.h"
+#include "ecal_config_reader_hlp.h"
+#include "ecal_def.h"
+
+namespace eCAL
+{
+  namespace Config
+  {
+    /////////////////////////////////////
+    // common
+    /////////////////////////////////////
+    
+    ECAL_API std::string GetLoadedEcalIniPath                 () { return g_default_ini_file; }
+    ECAL_API int         GetRegistrationTimeoutMs             () { return eCALPAR(CMN, REGISTRATION_TO); }
+    ECAL_API int         GetRegistrationRefreshMs             () { return eCALPAR(CMN, REGISTRATION_REFRESH); }
+
+    /////////////////////////////////////
+    // network
+    /////////////////////////////////////
+
+    ECAL_API bool        IsNetworkEnabled                     () { return eCALPAR(NET, ENABLED); }
+
+    ECAL_API std::string GetUdpMulticastGroup                 () { return eCALPAR(NET, UDP_MULTICAST_GROUP); }
+    ECAL_API std::string GetUdpMulticastMask                  () { return eCALPAR(NET, UDP_MULTICAST_MASK); }
+    ECAL_API int         GetUdpMulticastPort                  () { return eCALPAR(NET, UDP_MULTICAST_PORT); }
+    ECAL_API int         GetUdpMulticastTtl                   () { return eCALPAR(NET, UDP_MULTICAST_TTL); }
+
+    ECAL_API int         GetUdpMulticastSndBufSizeBytes       () { return eCALPAR(NET, UDP_MULTICAST_SNDBUF); }
+    ECAL_API int         GetUdpMulticastRcvBufSizeBytes       () { return eCALPAR(NET, UDP_MULTICAST_RCVBUF); }
+
+    ECAL_API int         GetMaxUdpBandwidthBytesPerSecond     () { return eCALPAR(NET, BANDWIDTH_MAX_UDP); }
+
+    ECAL_API bool        IsUdpMulticastRecEnabled             () { return eCALPAR(NET, UDP_MC_REC_ENABLED); }
+    ECAL_API bool        IsShmRecEnabled                      () { return eCALPAR(NET, SHM_REC_ENABLED); }
+    ECAL_API bool        IsTcpRecEnabled                      () { return eCALPAR(NET, TCP_REC_ENABLED); }
+    ECAL_API bool        IsInprocRecEnabled                   () { return eCALPAR(NET, INPROC_REC_ENABLED); }
+
+    ECAL_API bool        IsNpcapEnabled                       () { return eCALPAR(NET, NPCAP_ENABLED); }
+
+    ECAL_API int         GetTcpPubsubReaderThreadpoolSize     () { return eCALPAR(NET, TCP_PUBSUB_NUM_EXECUTOR_READER); }
+    ECAL_API int         GetTcpPubsubWriterThreadpoolSize     () { return eCALPAR(NET, TCP_PUBSUB_NUM_EXECUTOR_WRITER); }
+    ECAL_API int         GetTcpPubsubMaxReconnectionAttemps   () { return eCALPAR(NET, TCP_PUBSUB_MAX_RECONNECTIONS); }
+
+    /////////////////////////////////////
+    // time
+    /////////////////////////////////////
+    
+    ECAL_API std::string GetTimesyncModuleName                () { return eCALPAR(TIME,  SYNC_MOD_RT); }
+
+    /////////////////////////////////////
+    // process
+    /////////////////////////////////////
+    
+    ECAL_API std::string GetTerminalEmulatorCommand           () { return eCALPAR(PROCESS,  TERMINAL_EMULATOR); }
+
+    /////////////////////////////////////
+    // monitoring
+    /////////////////////////////////////
+    
+    ECAL_API int         GetMonitoringTimeoutMs               () { return eCALPAR(MON, TIMEOUT); }
+    ECAL_API std::string GetMonitoringFilterExcludeList       () { return eCALPAR(MON, FILTER_EXCL); }
+    ECAL_API std::string GetMonitoringFilterIncludeList       () { return eCALPAR(MON, FILTER_INCL); }
+    ECAL_API std::string GetConsoleLogFilter                  () { return eCALPAR(MON, LOG_FILTER_CON); }
+    ECAL_API std::string GetLogFileName                       () { return eCALPAR(MON, LOG_FILTER_FILE); }
+    ECAL_API std::string GetUdpLogFilter                      () { return eCALPAR(MON, LOG_FILTER_UDP); }
+
+    /////////////////////////////////////
+    // sys
+    /////////////////////////////////////
+    
+    //ECAL_API std::string GetEcalSysFilterExcludeList          () { return eCALPAR(SYS, FILTER_EXCL); }
+
+    /////////////////////////////////////
+    // publisher
+    /////////////////////////////////////
+
+    ECAL_API int         GetPublisherInprocMode               () { return eCALPAR(PUB, USE_UDP_MC); }
+    ECAL_API int         GetPublisherShmMode                  () { return eCALPAR(PUB, USE_SHM); }
+    ECAL_API int         GetPublisherTcpMode                  () { return eCALPAR(PUB, USE_TCP); }
+    ECAL_API int         GetPublisherUdpMulticastMode         () { return eCALPAR(PUB, USE_INPROC); }
+    ECAL_API int         GetMemfileMinsizeBytes               () { return eCALPAR(PUB, MEMFILE_MINSIZE); }
+    ECAL_API int         GetMemfileOverprovisioningPercentage () { return eCALPAR(PUB, MEMFILE_RESERVE); }
+    ECAL_API int         GetMemfileAckTimeoutMs               () { return eCALPAR(PUB, MEMFILE_ACK_TO); }
+    ECAL_API bool        IsMemfileZerocopyEnabled             () { return eCALPAR(PUB, MEMFILE_ZERO_COPY); }
+    ECAL_API int         GetMemfileBufferCount                () { return eCALPAR(PUB, MEMFILE_BUF_COUNT); }
+    ECAL_API bool        IsTopicTypeSharingEnabled            () { return eCALPAR(PUB, SHARE_TTYPE); }
+    ECAL_API int         IsTopicDescriptionSharingEnabled     () { return eCALPAR(PUB, SHARE_TDESC); }
+  }
+}
