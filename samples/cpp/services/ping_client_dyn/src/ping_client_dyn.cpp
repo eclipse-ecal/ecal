98533344c128f89738a202763e4b487c9575bd2a
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2022-04-14 12:34:23 +0200

    json/protobuf functionality excluded in separate unit

--- samples/cpp/services/ping_client_dyn/src/ping_client_dyn.cpp
+++ samples/cpp/services/ping_client_dyn/src/ping_client_dyn.cpp
@@ -18,84 +18,13 @@
 */
 
 #include <ecal/ecal.h>
-#include <ecal/protobuf/ecal_proto_dyn.h>
 
 #include <iostream>
 #include <chrono>
 #include <thread>
 
-#include <google/protobuf/util/json_util.h>
+#include "proto_json_conv.h"
 
-// helper function to create a serialized google message out of a message descriptor, a message type and a JSON message
-std::string GetSerialzedMessageFromJSON(const std::string& msg_desc_, const std::string& msg_type_, const std::string& msg_json_)
-{
-  // create file descriptor set
-  google::protobuf::FileDescriptorSet req_pset;
-  if (!req_pset.ParseFromString(msg_desc_))
-  {
-    std::cerr << "Could not create google file descriptor set." << std::endl;
-    return "";
-  }
-
-  // create message object
-  eCAL::protobuf::CProtoDynDecoder msg_decoder;
-  std::string error_s;
-  google::protobuf::Message* req_msg = msg_decoder.GetProtoMessageFromDescriptorSet(req_pset, msg_type_, error_s);
-  if(!req_msg)
-  {
-    std::cerr << "Could not create google message object." << std::endl;
-    return "";
-  }
-
-  // convert JSON request string into message
-  google::protobuf::util::Status status = google::protobuf::util::JsonStringToMessage(msg_json_, req_msg);
-  if (!status.ok())
-  {
-    std::cerr << "Could not convert JSON to google message." << std::endl;
-    return "";
-  }
-
-  return req_msg->SerializeAsString();
-}
-
-// helper function to create a serialized google message out of a message descriptor, a message type and a JSON message
-std::string GetJSONFromSerialzedMessage(const std::string& msg_desc_, const std::string& msg_type_, const std::string& msg_google_)
-{
-  // create file descriptor set
-  google::protobuf::FileDescriptorSet req_pset;
-  if (!req_pset.ParseFromString(msg_desc_))
-  {
-    std::cerr << "Could not create google file descriptor set." << std::endl;
-    return "";
-  }
-
-  // create message object
-  eCAL::protobuf::CProtoDynDecoder msg_decoder;
-  std::string error_s;
-  google::protobuf::Message* req_msg = msg_decoder.GetProtoMessageFromDescriptorSet(req_pset, msg_type_, error_s);
-  if (!req_msg)
-  {
-    std::cerr << "Could not create google message object." << std::endl;
-    return "";
-  }
-
-  if (!req_msg->ParseFromString(msg_google_))
-  {
-    std::cerr << "Could not parse google message content from string." << std::endl;
-    return "";
-  }
-
-  std::string msg_json;
-  google::protobuf::util::Status status = google::protobuf::util::MessageToJsonString(*req_msg, &msg_json);
-  if (!status.ok())
-  {
-    std::cerr << "Could not convert google message to JSON." << std::endl;
-    return "";
-  }
-
-  return msg_json;
-}
-  
 // main entry
 int main(int argc, char **argv)
 {
@@ -104,7 +33,7 @@ int main(int argc, char **argv)
 
   // create ping service client
   const std::string service_name("ping service");
-  const std::string method_name("Ping");
+  const std::string method_name ("Ping");
   eCAL::CServiceClient ping_client(service_name);
 
   // waiting for service
@@ -113,13 +42,14 @@ int main(int argc, char **argv)
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     std::cout << "Waiting for the service .." << std::endl;
   }
-  std::cout << std::endl;
 
   // get service method type names
   std::string req_type, resp_type;
   if (!eCAL::Util::GetServiceTypeNames(service_name, method_name, req_type, resp_type))
   {
     std::cerr << "Could not get service type names !" << std::endl;
+    eCAL::Finalize();
+    return -1;
   }
 
   // get service method type descriptions
@@ -127,6 +57,8 @@ int main(int argc, char **argv)
   if (!eCAL::Util::GetServiceDescription(service_name, method_name, req_desc, resp_desc))
   {
     std::cerr << "Could not get service type descriptions !" << std::endl;
+    eCAL::Finalize();
+    return -1;
   }
 
   int cnt(0);
@@ -157,8 +89,8 @@ int main(int argc, char **argv)
               // service successful executed
             case call_state_executed:
             {
-              std::string resp_json = GetJSONFromSerialzedMessage(req_desc, req_type, service_response.response);
-              std::cout << "Received response PingService / Ping (JSON) : " << resp_json << " from host " << service_response.host_name << std::endl;
+              std::string resp_json = GetJSONFromSerialzedMessage(resp_desc, resp_type, service_response.response);
+              std::cout << "Received response PingService / Ping         (JSON) : " << resp_json << " from host " << service_response.host_name << std::endl;
             }
             break;
             // service execution failed
