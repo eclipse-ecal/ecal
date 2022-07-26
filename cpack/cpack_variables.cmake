ee29fe7881d95d7336d6ee42834543c8c1e6e5ac
Emil Bic <18248171+emilbic28@users.noreply.github.com>
2022-07-26 10:59:25 +0200

    Apps: Added Mime-types and icons for eCAL Play, eCAL Sys and eCAL Rec files

--- cpack/cpack_variables.cmake
+++ cpack/cpack_variables.cmake
@@ -54,6 +54,7 @@ set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Rex Schilasky <rex.schilasky@continental-co
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://github.com/continental/ecal")
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 set(CPACK_DEBIAN_PACKAGE_GENERATE_SHLIBS ON)
+set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CMAKE_BINARY_DIR}/cpack/debscripts/postinst" "${CMAKE_BINARY_DIR}/cpack/debscripts/postrm")
 set(CPACK_DEBIAN_PACKAGE_DEBUG ON)
 
 
