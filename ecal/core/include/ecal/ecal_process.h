80e9caa9e2fc264877b37e1a88635569d119dd35
Emil-Adrian Bic <emilbic28@yahoo.ro>
2022-08-09 13:58:36 +0300

    Fixed ubuntu error

--- ecal/core/include/ecal/ecal_process.h
+++ ecal/core/include/ecal/ecal_process.h
@@ -82,22 +82,6 @@ namespace eCAL
     /**
      * @brief  Sleep current thread.
      *
-     * Templated implementation which takes as argument a std::chrono::duration and calls underlying SleepNS function.
-     * By using a std::chrono::duration argument we ensure that conversion to ms would be more precise for Windows Sleep method. 
-     *
-     * @param  time  Time to sleep expressed in std::chrono::duration.
-    **/
-
-    template <typename Rep, typename Period>
-    void SleepFor( std::chrono::duration<Rep, Period> time )
-    {
-        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(time).count();
-        SleepNS(ns);
-    }
-
-    /**
-     * @brief  Sleep current thread.
-     *
      * Because of the fact that std::this_thread::sleep_for is vulnerable to system clock changes
      * on Windows, Sleep function from synchapi.h had to be used for Windows. This insures time
      * robustness on all platforms from a thread sleep perspective.
@@ -116,6 +100,22 @@ namespace eCAL
      * @param  time_ms_  Time to sleep in ns.
     **/
     ECAL_API void SleepNS(const long long time_ns_);
+
+    /**
+     * @brief  Sleep current thread.
+     *
+     * Templated implementation which takes as argument a std::chrono::duration and calls underlying SleepNS function.
+     * By using a std::chrono::duration argument we ensure that conversion to ms would be more precise for Windows Sleep method. 
+     *
+     * @param  time  Time to sleep expressed in std::chrono::duration.
+    **/
+
+    template <typename Rep, typename Period>
+    void SleepFor( std::chrono::duration<Rep, Period> time )
+    {
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(time).count();
+        SleepNS(ns);
+    }
 
     /**
      * @brief  Get current process id. 
