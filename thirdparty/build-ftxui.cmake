2b1d8f9636265ff963eb0eaf5cac9000e12872b1
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2022-09-28 11:48:14 +0200

    Added Code that upgrades the MSVC Toolset to v142 for mon_cli / ftxui components

--- thirdparty/build-ftxui.cmake
+++ thirdparty/build-ftxui.cmake
@@ -1,3 +1,22 @@
 option(FTXUI_BUILD_EXAMPLES "Set to ON to build examples" OFF)
 
 add_subdirectory(thirdparty/ftxui)
+
+# Upgrade Visual studio toolset version if necessary. Minimum working version is v142
+if(MSVC AND NOT CMAKE_VS_PLATFORM_TOOLSET STREQUAL "")
+  string(SUBSTRING "${CMAKE_VS_PLATFORM_TOOLSET}" 1 -1 VS_TOOLSET_NUMBER)
+  if ("${VS_TOOLSET_NUMBER}" LESS "142")
+    set_target_properties(component
+      PROPERTIES
+        VS_PLATFORM_TOOLSET v142
+    )
+    set_target_properties(screen
+      PROPERTIES
+        VS_PLATFORM_TOOLSET v142
+    )
+    set_target_properties(dom
+      PROPERTIES
+        VS_PLATFORM_TOOLSET v142
+    )
+  endif()
+endif()
