d3be24e9200f1d19dfa88fb717239bbb1077f3f5
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2022-09-30 16:43:03 +0200

    Added code to install vcruntime140_1.dll

--- thirdparty/build-ftxui.cmake
+++ thirdparty/build-ftxui.cmake
@@ -3,6 +3,7 @@ option(FTXUI_BUILD_EXAMPLES "Set to ON to build examples" OFF)
 add_subdirectory(thirdparty/ftxui)
 
 # Upgrade Visual studio toolset version if necessary. Minimum working version is v142
+# TODO: Remove this code block as soon as possible
 if (MSVC AND (MSVC_TOOLSET_VERSION LESS 142))
   set_target_properties(component
     PROPERTIES
