4133ec3429096fd389afeb35b903e60e1f7a9f09
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2022-10-20 11:28:28 +0200

    Revert Changes from conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -6,41 +6,38 @@ import pathlib
 
 class eCALConan(ConanFile):
     name = "ecal"
-    settings = "os", "compiler", "arch"
+    settings = "os", "compiler", "arch", "build_type"
     license = "BSD-3-Clause"
     generators = "virtualenv"
-    python_requires = {"cmake-presets-generator/0.1.0@heat-rae/stable", "conancontifile/0.0.15@heat-rae/stable"}
+    build_requires = "cmake/3.21.1"
     
     def build_requirements(self):
-        self.build_requires("doxygen/[*]@heat-rae/stable")
-        self.build_requires("cmake/[*]@heat-rae/stable")
+        self.build_requires("doxygen/1.9.1")
     
     def requirements(self):
-        self.requires("termcolor/[*]@heat-rae/stable")
-        self.requires("fineftp-server/[*]@heat-rae/stable")
-        self.requires("curl/[*]@heat-rae/stable")
-        self.requires("hdf5/[*]@heat-rae/stable")
-        self.requires("protobuf/[*]@heat-rae/stable")
-        self.requires("curl/[*]@heat-rae/stable")
-        self.requires("qt/[*]@heat-rae/stable")
-        self.requires("spdlog/[*]@heat-rae/stable")
-        self.requires("tclap/[*]@heat-rae/stable")
-        self.requires("asio/[*]@heat-rae/stable")
-        self.requires("gtest/[*]@heat-rae/stable")
-        self.requires("tinyxml2/[*]@heat-rae/stable")
-        self.requires("openssl/1.1.1i@heat-rae/stable")
-        self.requires("tcp-pubsub/[*]@heat-rae/stable")
-        self.requires("recycle/[*]@heat-rae/stable")
-        if self.settings.os == "Windows":
-            self.requires("npcap/[*]@heat-rae/stable")
-            # We need to fix this version, sorry!
-            self.requires("pcapplusplus/21.05@heat-rae/stable")
+        self.requires("hdf5/1.10.6")
+        self.requires("protobuf/3.17.1")
+        self.requires("libcurl/7.78.0")
+        self.requires("qt/5.15.2")
+        self.requires("spdlog/1.9.2")
+        #self.requires("tclap/1.2.4")
+        #self.requires("asio/1.19.2")
+        self.requires("gtest/1.11.0")
+        self.requires("tinyxml2/8.0.0")
+        self.requires("openssl/1.1.1l", override=True)
         
-       
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.options["qt"].shared = True
+            self.options["qt"].qtwinextras = True
+        
     def generate(self):
-        CMakePresets = self.python_requires["cmake-presets-generator"].module.CMakePresets
-        cmake_folder = self.recipe_folder
-        tc = CMakePresets(self, cmake_folder=".")
+        # Don't generate the config files for doxygen. They don't work properlyexit
+        self.deps_cpp_info["doxygen"].set_property("cmake_find_mode", "none")
+        cmake = CMakeDeps(self)
+        cmake.generate()
+        
+        tc = CMakeToolchain(self)
         # customize toolchain "tc"
         tc.variables["ECAL_THIRDPARTY_BUILD_CURL"] = "OFF"
         tc.variables["ECAL_THIRDPARTY_BUILD_SPDLOG"] = "OFF"
@@ -48,16 +45,12 @@ class eCALConan(ConanFile):
         tc.variables["ECAL_THIRDPARTY_BUILD_PROTOBUF"] = "OFF"
         tc.variables["ECAL_THIRDPARTY_BUILD_HDF5"] = "OFF"
         tc.variables["ECAL_THIRDPARTY_BUILD_TINYXML2"] = "OFF"
-        tc.variables["ECAL_THIRDPARTY_BUILD_TCP_PUBSUB"] = "OFF"
-        tc.variables["ECAL_THIRDPARTY_BUILD_RECYCLE"] = "OFF"
-        tc.variables["ECAL_THIRDPARTY_BUILD_FINEFTP"] = "OFF"
-        tc.variables["ECAL_THIRDPARTY_BUILD_TERMCOLOR"] = "OFF"
-
-        if self.settings.os == "Windows":
-            tc.variables["ECAL_NPCAP_SUPPORT"] = "ON"
         tc.variables["BUILD_DOCS"] = "ON"
-        tc.variables["BUILD_ECAL_TESTS"] = "ON"
         tc.variables["CMAKE_FIND_PACKAGE_PREFER_CONFIG"] = "ON"
+        if self.settings.os == "Windows":
+            tc.variables["Protobuf_PROTOC_EXECUTABLE"] = os.path.join(self.deps_cpp_info["protobuf"].rootpath, "bin", "protoc.exe").replace('\\', '/')
+        else:
+            tc.variables["Protobuf_PROTOC_EXECUTABLE"] = os.path.join(self.deps_cpp_info["protobuf"].rootpath, "bin", "protoc")
         tc.generate()
         
         
