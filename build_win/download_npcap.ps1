750791f866abc3d240718cbe9334b5e03873fe32
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2022-11-25 16:20:58 +0100

    Replaced internal Udpcap with a submodule (#884)
    * Added Udpcap dependency from external repository
    * npcap / pcap++ are fetched by udpcap or can be pre-downloaded 
    
    Co-authored-by: Kristof Hannemann <50989282+hannemn@users.noreply.github.com>

--- build_win/download_npcap.ps1
+++ build_win/download_npcap.ps1
@@ -1,11 +1,9 @@
 
 $target_dir = "$PSScriptRoot\..\thirdparty\npcap\"
+
 New-Item "$target_dir" -ItemType "directory" -Force
 
 $webclient = New-Object System.Net.WebClient
 
-$webclient.DownloadFile("https://nmap.org/npcap/dist/npcap-sdk-1.10.zip","$target_dir\npcap-sdk-1.10.zip")
-$webclient.DownloadFile("https://github.com/seladb/PcapPlusPlus/releases/download/v21.05/pcapplusplus-21.05-windows-vs2015.zip","$target_dir\pcapplusplus-21.05-windows-vs2015.zip")
-
-Expand-Archive -Force -Path "$target_dir\npcap-sdk-1.10.zip" -DestinationPath "$target_dir\npcap-sdk"
-Expand-Archive -Force -Path "$target_dir\pcapplusplus-21.05-windows-vs2015.zip" -DestinationPath "$target_dir"
+$webclient.DownloadFile("https://npcap.com/dist/npcap-sdk-1.13.zip","$target_dir\npcap-sdk.zip")
+$webclient.DownloadFile("https://github.com/seladb/PcapPlusPlus/releases/download/v22.05/pcapplusplus-22.05-windows-vs2015.zip","$target_dir\pcapplusplus.zip")
