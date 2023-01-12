eclipse-ecal/ecal

commit 419d7f33edca6cf4b02c50b8e879981f4043f873

Core Services: Fixed bug that caused the monitoring loop to block in case of long running RPC calls (#906)
- Also fixed a small bug that would prevent an error to be properly propagated to the caller
- Added GTests

Co-authored-by: rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>

ecal/core/src/service/ecal_service_server_impl.cpp
@@ -279,50 +279,79 @@ namespace eCAL
 
   int CServiceServerImpl::RequestCallback(const std::string& request_, std::string& response_)
   {
-    std::lock_guard<std::mutex> lock(m_method_map_sync);
-
-    if (m_method_map.empty()) return 0;
-
-    int success(-1);
+    // prepare response
     eCAL::pb::Response response_pb;
-    auto response_pb_mutable_header = response_pb.mutable_header();
+    auto* response_pb_mutable_header = response_pb.mutable_header();
     response_pb_mutable_header->set_hname(eCAL::Process::GetHostName());
     response_pb_mutable_header->set_sname(m_service_name);
     response_pb_mutable_header->set_sid(m_service_id);
 
-    eCAL::pb::Request request_pb;
-    if (request_pb.ParseFromString(request_))
+    // try to parse request
+    eCAL::pb::Request  request_pb;
+    if (!request_pb.ParseFromString(request_))
     {
-      // success == 0 means we could deserialize and
-      // no need to try to read more bytes for the server
-      // socket
-      success = 0;
-      auto request_pb_header = request_pb.header();
-      response_pb_mutable_header->set_mname(request_pb_header.mname());
+      Logging::Log(log_level_error, m_service_name + "::CServiceServerImpl::RequestCallback failed to parse request message");
 
-      auto iter = m_method_map.find(request_pb_header.mname());
-      if (iter != m_method_map.end())
+      response_pb_mutable_header->set_state(eCAL::pb::ServiceHeader_eCallState_failed);
+      std::string emsg = "Service '" + m_service_name + "' request message could not be parsed.";
+      response_pb_mutable_header->set_error(emsg);
+
+      // serialize response and return "request message could not be parsed"
+      response_ = response_pb.SerializeAsString();
+      
+      // return value is not propagated to the remote caller.
+      return 0;
+    }
+
+    // get method
+    SMethod method;
+    auto& request_pb_header = request_pb.header();
+    {
+      std::lock_guard<std::mutex> lock(m_method_map_sync);
+
+      auto requested_method_iterator = m_method_map.find(request_pb_header.mname());
+      if (requested_method_iterator == m_method_map.end())
       {
-        auto call_count = iter->second.method_pb.call_count();
-        iter->second.method_pb.set_call_count(++call_count);
+        // set method call state 'failed'
+        response_pb_mutable_header->set_state(eCAL::pb::ServiceHeader_eCallState_failed);
+        // set error message
+        std::string emsg = "Service '" + m_service_name + "' has no method named '" + request_pb_header.mname() + "'";
+        response_pb_mutable_header->set_error(emsg);
 
-        std::string request_s = request_pb.request();
-        std::string response_s;
-        int service_return_state = m_method_map[request_pb_header.mname()].callback(iter->second.method_pb.mname(), iter->second.method_pb.req_type(), iter->second.method_pb.resp_type(), request_s, response_s);
+        // serialize response and return "method not found"
+        response_ = response_pb.SerializeAsString();
 
-        response_pb_mutable_header->set_state(eCAL::pb::ServiceHeader_eCallState_executed);
-        response_pb.set_response(response_s);
-        response_pb.set_ret_state(service_return_state);
+        // Return Success (error_code = 0), as parsing the request worked. The
+        // return value is not propagated to the remote caller.
+        return 0;
       }
       else
       {
-        response_pb_mutable_header->set_state(eCAL::pb::ServiceHeader_eCallState_failed);
-        std::string emsg = "Service " + m_service_name + " has no method " + request_pb_header.mname();
-        response_pb_mutable_header->set_error(emsg);
+        // increase call count
+        auto call_count = requested_method_iterator->second.method_pb.call_count();
+        requested_method_iterator->second.method_pb.set_call_count(++call_count);
+
+        // store (copy) the method object, so we can release the mutex before calling the function
+        method = requested_method_iterator->second;
       }
-      response_ = response_pb.SerializeAsString();
     }
-    return success;
+
+    // execute method (outside lock guard)
+    const std::string& request_s = request_pb.request();
+    std::string response_s;
+    int service_return_state = method.callback(method.method_pb.mname(), method.method_pb.req_type(), method.method_pb.resp_type(), request_s, response_s);
+
+    // set method call state 'executed'
+    response_pb_mutable_header->set_state(eCAL::pb::ServiceHeader_eCallState_executed);
+    // set method response and return state
+    response_pb.set_response(response_s);
+    response_pb.set_ret_state(service_return_state);
+
+    // serialize response and return
+    response_ = response_pb.SerializeAsString();
+
+    // return success (error code 0)
+    return 0;
   }
 
   void CServiceServerImpl::EventCallback(eCAL_Server_Event event_, const std::string& /*message_*/)

ecal/core/src/service/ecal_service_client_impl.cpp
@@ -424,12 +424,12 @@ namespace eCAL
         {
           // execute request
           SServiceResponse service_response;
-          bool ret = SendRequest(client.second, method_name_, request_, timeout_, service_response);
-          if (ret == false)
+          ret_state = SendRequest(client.second, method_name_, request_, timeout_, service_response);
+          if (ret_state == false)
           {
             std::cerr << "CServiceClientImpl::SendRequests failed." << std::endl;
-            return false;
           }
+          
           // call response callback
           if (service_response.call_state != call_state_none)
           {

ecal/core/src/service/ecal_service_server_impl.h
@@ -83,6 +83,14 @@ namespace eCAL
     CServiceServerImpl& operator=(const CServiceServerImpl&) = delete;
 
   protected:
+    /**
+     * @brief Calls the request callback based on the request and fills the response
+     * 
+     * @param[in]  request_   The service request in serialized protobuf form
+     * @param[out] response_  A serialized protobuf response. My not be set at all.
+     * 
+     * @return always 0.
+     */
     int RequestCallback(const std::string& request_, std::string& response_);
     void EventCallback(eCAL_Server_Event event_, const std::string& message_);
 

testing/ecal/clientserver_test/CMakeLists.txt
@@ -24,7 +24,7 @@ find_package(Protobuf REQUIRED)
 
 create_targets_protobuf()
 
-set(clientserver_test_src
+set(${PROJECT_NAME}_src
   src/clientserver_test.cpp
 )
 
@@ -33,14 +33,17 @@ set(clientserver_test_proto
   ${CMAKE_CURRENT_SOURCE_DIR}/src/protobuf/ping.proto
 )
 
-ecal_add_static_library(clientserver_proto src/dummy.cpp)
-PROTOBUF_TARGET(clientserver_proto ${CMAKE_CURRENT_SOURCE_DIR}/src/protobuf ${clientserver_test_proto})
-target_link_libraries(clientserver_proto PRIVATE protobuf::libprotobuf)
-set_property(TARGET clientserver_proto PROPERTY FOLDER testing/ecal/service)
+ecal_add_gtest(${PROJECT_NAME} ${${PROJECT_NAME}_src})
+PROTOBUF_TARGET_CPP(${PROJECT_NAME} ${CMAKE_CURRENT_SOURCE_DIR}/src/protobuf ${clientserver_test_proto})
 
-ecal_add_gtest(${PROJECT_NAME} ${clientserver_test_src})
 target_link_libraries(${PROJECT_NAME}
-  PRIVATE eCAL::core clientserver_proto Threads::Threads)
+  PRIVATE
+    eCAL::core
+    protobuf::libprotobuf
+    Threads::Threads)
 
-  ecal_install_gtest(${PROJECT_NAME})
+target_compile_features(${PROJECT_NAME} PRIVATE cxx_std_14)
+
+ecal_install_gtest(${PROJECT_NAME})
+
 set_property(TARGET ${PROJECT_NAME} PROPERTY FOLDER testing/ecal/service)

testing/ecal/clientserver_test/src/clientserver_test.cpp
@@ -147,12 +147,6 @@ TEST(IO, ClientConnectEvent)
   // informed about disconnection without calling a service method
   client.Call("foo", "");
   eCAL::Process::SleepMS(1000);
-  EXPECT_EQ(1, event_disconnected_fired);
-  client.Call("foo", "");
-  eCAL::Process::SleepMS(1000);
-  EXPECT_EQ(2, event_disconnected_fired);
-  client.Call("foo", "");
-  eCAL::Process::SleepMS(1000);
   EXPECT_EQ(2, event_disconnected_fired);
 
   // finalize eCAL API

commit 74bc36f887e7db017ea3d9ed66a8fe92cc8a5eed

Core Services Bugfix: Service response header now contains the method name again
Fixes #937, a bug that was introduced with commit 419d7f33edca6cf4b02c50b8e879981f4043f873

ecal/core/src/service/ecal_service_server_impl.cpp
@@ -306,6 +306,7 @@ namespace eCAL
     // get method
     SMethod method;
     auto& request_pb_header = request_pb.header();
+    response_pb_mutable_header->set_mname(request_pb_header.mname());
     {
       std::lock_guard<std::mutex> lock(m_method_map_sync);
 