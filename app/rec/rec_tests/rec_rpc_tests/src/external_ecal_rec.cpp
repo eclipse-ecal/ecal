be4cdd747331c72d293fb884d850107b8d08dca3
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2023-01-31 11:10:29 +0100

    Fixed issue on linux

--- app/rec/rec_tests/rec_rpc_tests/src/external_ecal_rec.cpp
+++ app/rec/rec_tests/rec_rpc_tests/src/external_ecal_rec.cpp
@@ -46,12 +46,12 @@ ExternalEcalRecInstance::ExternalEcalRecInstance(bool gui)
   if (gui)
   {
     std::cout << "Starting " << "\"" << ECAL_REC_GUI_PATH << "\"" << std::endl;
-    pid = eCAL::Process::StartProcess(("\"" + std::string(ECAL_REC_GUI_PATH) + "\"").c_str(), "", "", false, eCAL_Process_eStartMode::proc_smode_minimized, false);
+    pid = eCAL::Process::StartProcess(ECAL_REC_GUI_PATH, "", "", false, eCAL_Process_eStartMode::proc_smode_minimized, false);
   }
   else
   {
     std::cout << "Starting " << "\"" << ECAL_REC_CLI_PATH << "\"" << std::endl;
-    pid = eCAL::Process::StartProcess(("\"" + std::string(ECAL_REC_CLI_PATH) + "\"").c_str(), "--interactive-dont-exit --no-default", "", false, eCAL_Process_eStartMode::proc_smode_hidden, false);
+    pid = eCAL::Process::StartProcess(ECAL_REC_CLI_PATH, "--interactive-dont-exit --no-default", "", false, eCAL_Process_eStartMode::proc_smode_hidden, false);
   }
 
   std::this_thread::sleep_for(std::chrono::milliseconds(1500));
