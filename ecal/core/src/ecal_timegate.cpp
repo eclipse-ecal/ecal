eec4769b0c0a2f109558e60e7bd231e0f68db08e
JerryHyun <hsh858@gmail.com>
2023-02-28 14:42:52 +0900

    Fix: time-related dylib were not found on macOS

--- ecal/core/src/ecal_timegate.cpp
+++ ecal/core/src/ecal_timegate.cpp
@@ -47,9 +47,9 @@
 
 #define ecal_time_plugin_dir             "ecaltime_plugins"
 
-#ifdef __linux__
+#ifdef ECAL_OS_LINUX
 #include <dlfcn.h>
-#endif //__linux__
+#endif // ECAL_OS_LINUX
 
 namespace eCAL
 {
@@ -375,8 +375,12 @@ namespace eCAL
       module_name = "lib" + module_name + ".so";
 #endif
 
-    if (!interface_.module_handle)
-    {
+#ifdef __APPLE__
+      module_name = "lib" + module_name + ".dylib";
+#endif //__APPLE__
+
+      if (!interface_.module_handle)
+      {
 #ifdef _WIN32
       // try to load plugin from paths that are specified in the time plugin environment variable
       for (const auto& ecal_time_plugin_path : ecal_time_plugin_paths)
@@ -399,8 +403,8 @@ namespace eCAL
         const auto module_path = std::string(ecal_time_plugin_dir) + "\\" + module_name;
         interface_.module_handle = LoadLibrary(module_path.c_str());
       }
-#endif
-#ifdef __linux__
+#endif // __WIN32
+#ifdef ECAL_OS_LINUX
       // try to load plugin from paths that are specified in the time plugin environment variable
       for (const auto& ecal_time_plugin_path : ecal_time_plugin_paths)
       {
@@ -415,7 +419,7 @@ namespace eCAL
         const auto module_path = module_name;
         interface_.module_handle = dlopen(module_path.c_str(), RTLD_NOW);
       }
-#endif
+#endif // ECAL_OS_LINUX
 
       if (!interface_.module_handle)
       {
@@ -435,7 +439,7 @@ namespace eCAL
         interface_.etime_sleep_for_nanoseconds_ptr = (etime_sleep_for_nanoseconds) GetProcAddress(interface_.module_handle, etime_sleep_for_nanoseconds_name);
         interface_.etime_get_status_ptr       = (etime_get_status)                 GetProcAddress(interface_.module_handle, etime_get_status_name);
 #endif // _WIN32
-#ifdef __linux__
+#ifdef ECAL_OS_LINUX
         interface_.module_name               = module_name;
         interface_.etime_initialize_ptr      = (etime_initialize)                  dlsym(interface_.module_handle, etime_initialize_name);
         interface_.etime_finalize_ptr        = (etime_finalize)                    dlsym(interface_.module_handle, etime_finalize_name);
@@ -445,7 +449,7 @@ namespace eCAL
         interface_.etime_is_master_ptr       = (etime_is_master)                   dlsym(interface_.module_handle, etime_is_master_name);
         interface_.etime_sleep_for_nanoseconds_ptr = (etime_sleep_for_nanoseconds) dlsym(interface_.module_handle, etime_sleep_for_nanoseconds_name);
         interface_.etime_get_status_ptr      = (etime_get_status)                  dlsym(interface_.module_handle, etime_get_status_name);
-#endif // __linux__
+#endif // ECAL_OS_LINUX
 
         if (  (interface_.etime_initialize_ptr            == nullptr)
            || (interface_.etime_finalize_ptr              == nullptr)
