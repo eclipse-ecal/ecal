4697c12c8c3cae221966402388542bbdce8f78eb
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2023-05-09 14:39:57 +0200

    Upgrade github actions ecal build to v142, and remove C++17 hack.

--- thirdparty/build-ftxui.cmake
+++ thirdparty/build-ftxui.cmake
@@ -1,20 +1,3 @@
 option(FTXUI_BUILD_EXAMPLES "Set to ON to build examples" OFF)
 
 add_subdirectory(thirdparty/ftxui EXCLUDE_FROM_ALL)
-
-# Upgrade Visual studio toolset version if necessary. Minimum working version is v142
-# TODO: Remove this code block as soon as possible
-if (MSVC AND (MSVC_TOOLSET_VERSION LESS 142))
-  set_target_properties(component
-    PROPERTIES
-      VS_PLATFORM_TOOLSET v142
-  )
-  set_target_properties(screen
-    PROPERTIES
-      VS_PLATFORM_TOOLSET v142
-  )
-  set_target_properties(dom
-    PROPERTIES
-      VS_PLATFORM_TOOLSET v142
-  )
-endif()
