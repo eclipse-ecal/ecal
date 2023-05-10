d42cf9c5ce4888ea000c77e60e1c41d657f6fe6c
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2023-05-10 12:14:52 +0200

    Do not install meas targets / prefix mcap include directories correctly.

--- thirdparty/build-mcap.cmake
+++ thirdparty/build-mcap.cmake
@@ -4,7 +4,11 @@ find_package(lz4 REQUIRED)
 add_library(mcap INTERFACE)
 add_library(mcap::mcap ALIAS mcap)
 
-target_include_directories(mcap INTERFACE ${CMAKE_CURRENT_LIST_DIR}/mcap/cpp/mcap/include)
+#let's not worry about install at this very moment!-> 
+target_include_directories(mcap 
+  INTERFACE 
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/mcap/cpp/mcap/include>
+)
 target_compile_features(mcap INTERFACE cxx_std_17)
 target_link_libraries(mcap INTERFACE zstd::libzstd_static LZ4::lz4_static)
 
