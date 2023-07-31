2889182e18aa744405d15fb1e3829cb63141554e
Rex Schilasky <49162693+rex-schilasky@users.noreply.github.com>
2023-07-31 16:31:27 +0200

    Core / ZeroCopy: Added Doxygen API documentation and 2 new c++ samples demonstrating usage of low level zero copy API (#1145)
    Co-authored-by: Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>

---
+++ samples/cpp/pubsub/binary/binary_zero_copy_rec/src/binary_zero_copy_rec.cpp
@@ -0,0 +1,96 @@
+/* ========================= eCAL LICENSE =================================
+ *
+ * Copyright (C) 2016 - 2019 Continental Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================= eCAL LICENSE =================================
+*/
+
+#include <ecal/ecal.h>
+
+#include <iostream>
+#include <chrono>
+#include <sstream>
+#include <thread>
+
+// a simple struct to demonstrate
+// zero copy modifications
+struct alignas(4) SSimpleStruct
+{
+  uint32_t version      = 1;
+  uint16_t rows         = 5;
+  uint16_t cols         = 3;
+  uint32_t clock        = 0;
+  uint8_t  bytes[5 * 3] = { 0 };
+};
+
+// SSimpleStruct logging
+std::ostream& operator<<(std::ostream& os, const SSimpleStruct& s)
+{
+  os << "Version : " << s.version << std::endl;
+  os << "Rows    : " << s.rows    << std::endl;
+  os << "Cols    : " << s.cols    << std::endl;
+  os << "Clock   : " << s.clock   << std::endl;
+
+  os << "Bytes   : " << std::endl;
+  for (int i = 0; i < s.rows; ++i) {
+    for (int j = 0; j < s.cols; ++j) {
+      os << static_cast<int>(s.bytes[i * s.cols + j]) << " ";
+    }
+    os << std::endl;
+  }
+  return os;
+}
+
+// subscriber callback function
+void OnReceive(const char* /*topic_name_*/, const struct eCAL::SReceiveCallbackData* data_)
+{
+  if (data_->size < 1) return;
+
+  std::cout << "------------------------------------"   << std::endl;
+  std::cout << "Binary buffer header :"                 << std::endl;
+  std::cout << "------------------------------------"   << std::endl;
+  std::cout << " Size   : " << data_->size              << std::endl;
+  std::cout << " Id     : " << data_->id                << std::endl;
+  std::cout << " Time   : " << data_->time              << std::endl;
+  std::cout << " Clock  : " << data_->clock             << std::endl;
+  std::cout                                             << std::endl;
+  std::cout << "------------------------------------"   << std::endl;
+  std::cout << "SSimpleStruct :"                        << std::endl;
+  std::cout << "------------------------------------"   << std::endl;
+  std::cout << *static_cast<SSimpleStruct*>(data_->buf) << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+  const char* nodeName  = "binary_zero_copy_rec";
+  const char* topicName = "simple_struct";
+
+  // initialize eCAL API
+  eCAL::Initialize(argc, argv, nodeName);
+
+  // create the subscriber
+  eCAL::CSubscriber sub(topicName);
+
+  // assign callback
+  sub.AddReceiveCallback(OnReceive);
+
+  // idle main loop
+  while (eCAL::Ok()) std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+  // finalize eCAL API
+  eCAL::Finalize();
+
+  return 0;
+}
---
+++ samples/cpp/pubsub/binary/binary_zero_copy_snd/src/binary_zero_copy_snd.cpp
@@ -0,0 +1,146 @@
+/* ========================= eCAL LICENSE =================================
+ *
+ * Copyright (C) 2016 - 2019 Continental Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================= eCAL LICENSE =================================
+*/
+
+#include <ecal/ecal.h>
+
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <thread>
+
+// a simple struct to demonstrate
+// zero copy modifications
+struct alignas(4) SSimpleStruct
+{
+  uint32_t version      = 1;
+  uint16_t rows         = 5;
+  uint16_t cols         = 3;
+  uint32_t clock        = 0;
+  uint8_t  bytes[5 * 3] = { 0 };
+};
+
+// SSimpleStruct logging
+void log_struct(const char* action_name, const SSimpleStruct& s)
+{
+  std::cout << "------------------------------------" << std::endl;
+  std::cout << action_name << std::endl;
+  std::cout << "------------------------------------" << std::endl;
+
+  std::cout << "Version : " << s.version << std::endl;
+  std::cout << "Rows    : " << s.rows    << std::endl;
+  std::cout << "Cols    : " << s.cols    << std::endl;
+  std::cout << "Clock   : " << s.clock   << std::endl;
+
+  std::cout << "Bytes   : " << std::endl;
+  for (int i = 0; i < s.rows; ++i) {
+    for (int j = 0; j < s.cols; ++j) {
+      std::cout << static_cast<int>(s.bytes[i * s.cols + j]) << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+// a binary payload object that handles
+// SSimpleStruct Write and Update functionality
+class CStructPayload : public eCAL::CPayloadWriter
+{
+public:
+  // Write the complete SSimpleStruct to the shared memory
+  bool Write(void* buf_, size_t len_) override
+  {
+    // check available size and pointer
+    if (len_ < GetSize() || buf_ == nullptr) return false;
+
+    // create a new struct and update its content
+    SSimpleStruct simple_struct;
+    UpdateStruct(&simple_struct);
+
+    // copy complete struct into the memory
+    *static_cast<SSimpleStruct*>(buf_) = simple_struct;
+
+    // log action
+    log_struct("Write SSimpleStruct :", simple_struct);
+
+    return true;
+  };
+
+  // Modify the SSimpleStruct in the shared memory
+  bool Update(void* buf_, size_t len_) override
+  {
+    // check available size and pointer
+    if (len_ < GetSize() || buf_ == nullptr) return false;
+
+    // update the struct in memory
+    UpdateStruct(static_cast<SSimpleStruct*>(buf_));
+
+    // log action
+    log_struct("Update SSimpleStruct :", *static_cast<SSimpleStruct*>(buf_));
+
+    return true;
+  };
+
+  size_t GetSize() override { return sizeof(SSimpleStruct); };
+
+private:
+  void UpdateStruct(SSimpleStruct* simple_struct)
+  {
+    // modify the simple_struct
+    simple_struct->clock = clock;
+    for (auto i = 0; i < (simple_struct->rows * simple_struct->cols); ++i)
+    {
+      simple_struct->bytes[i] = static_cast<char>(simple_struct->clock);
+    }
+
+    // increase internal state clock
+    clock++;
+  };
+
+  uint32_t clock = 0;
+};
+
+int main(int argc, char** argv)
+{
+  const char* nodeName       = "binary_zero_copy_snd";
+  const char* topicName      = "simple_struct";
+  const char* structTypeName = "SSimpleStruct";
+
+  // initialize eCAL API
+  eCAL::Initialize(argc, argv, nodeName);
+
+  // create the publisher
+  eCAL::CPublisher pub(topicName, { "custom", structTypeName, "" });
+
+  // turn zero copy mode on
+  pub.ShmEnableZeroCopy(true);
+
+  // create the simple struct payload
+  CStructPayload struct_payload;
+
+  // send updates every 100 ms
+  while (eCAL::Ok())
+  {
+    pub.Send(struct_payload);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  // finalize eCAL API
+  eCAL::Finalize();
+
+  return 0;
+}
