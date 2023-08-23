b2739f12d0755ed95e0da4e2c59eb3f820705691
KerstinKeller <KerstinKeller@users.noreply.github.com>
2023-08-23 14:51:37 +0200

    #1186 App, Dependencies: Make compatible with ftxui 5.0.0 (#1188)
    * Make version info of ftxui available to build.

--- thirdparty/build-ftxui.cmake
+++ thirdparty/build-ftxui.cmake
@@ -1,3 +1,12 @@
 option(FTXUI_BUILD_EXAMPLES "Set to ON to build examples" OFF)
 
 add_subdirectory(thirdparty/ftxui EXCLUDE_FROM_ALL)
+
+# Set ftxui_VERSION_MAJOR, because it's only defined in the subdirectory scope and we cannot access it
+# Reading it automatically is less error prone than setting itt by hand
+file(READ thirdparty/ftxui/CMakeLists.txt content)
+if(content MATCHES "VERSION ([0-9]+)\\.[0-9]+\\.[0-9]+")
+  set(ftxui_VERSION_MAJOR "${CMAKE_MATCH_1}")
+else()
+  message(FATAL_ERROR "Couldn't read version info")
+endif()
