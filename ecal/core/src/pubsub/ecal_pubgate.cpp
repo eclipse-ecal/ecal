eclipse-ecal/ecal

commit 1dd4624515a645688f18756c97891ad078824cb8

core: fast unregistration for pub/sub and client/server (#1086)

ecal/core/src/pubsub/ecal_pubgate.cpp
@@ -21,9 +21,6 @@
  * @brief  eCAL publisher gateway class
 **/
 
-#include <ecal/ecal.h>
-
-#include "ecal_def.h"
 #include "ecal_config_reader_hlp.h"
 #include "ecal_pubgate.h"
 #include "ecal_descgate.h"
@@ -41,7 +38,7 @@ namespace eCAL
     m_share_type(true),
     m_share_desc(true)
   {
-  };
+  }
 
   CPubGate::~CPubGate()
   {
@@ -60,7 +57,7 @@ namespace eCAL
     if(!m_created) return;
 
     // destroy all remaining publisher
-    std::unique_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    const std::unique_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     for (auto iter = m_topic_name_datawriter_map.begin(); iter != m_topic_name_datawriter_map.end(); ++iter)
     {
       iter->second->Destroy();
@@ -84,7 +81,7 @@ namespace eCAL
     if(!m_created) return(false);
 
     // register writer and multicast group
-    std::unique_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    const std::unique_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     m_topic_name_datawriter_map.emplace(std::pair<std::string, CDataWriter*>(topic_name_, datawriter_));
 
     return(true);
@@ -95,13 +92,14 @@ namespace eCAL
     if(!m_created) return(false);
     bool ret_state = false;
 
-    std::unique_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    const std::unique_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     auto res = m_topic_name_datawriter_map.equal_range(topic_name_);
     for(TopicNameDataWriterMapT::iterator iter = res.first; iter != res.second; ++iter)
     {
       if(iter->second == datawriter_)
       {
         m_topic_name_datawriter_map.erase(iter);
+        ret_state = true;
         break;
       }
     }
@@ -113,15 +111,15 @@ namespace eCAL
   {
     if(!m_created) return;
 
-    const auto& ecal_sample_topic = ecal_sample_.topic();
-    const std::string& topic_name = ecal_sample_topic.tname();
-    const std::string& topic_id   = ecal_sample_topic.tid();
-    const std::string& topic_type = ecal_sample_topic.ttype();
-    const std::string& topic_desc = ecal_sample_topic.tdesc();
-    const std::string  process_id = std::to_string(ecal_sample_topic.pid());
+    const auto& ecal_sample = ecal_sample_.topic();
+    const std::string& topic_name = ecal_sample.tname();
+    const std::string& topic_id   = ecal_sample.tid();
+    const std::string& topic_type = ecal_sample.ttype();
+    const std::string& topic_desc = ecal_sample.tdesc();
+    const std::string  process_id = std::to_string(ecal_sample.pid());
 
     std::string reader_par;
-    for (const auto& layer : ecal_sample_topic.tlayer())
+    for (const auto& layer : ecal_sample.tlayer())
     {
       // layer parameter as protobuf message
       // this parameter is not used at all currently
@@ -133,7 +131,7 @@ namespace eCAL
     ApplyTopicToDescGate(topic_name, topic_type, topic_desc);
 
     // register local subscriber
-    std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    const std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     auto res = m_topic_name_datawriter_map.equal_range(topic_name);
     for(TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
     {
@@ -141,20 +139,38 @@ namespace eCAL
     }
   }
 
+  void CPubGate::ApplyLocSubUnregistration(const eCAL::pb::Sample& ecal_sample_)
+  {
+    if (!m_created) return;
+
+    const auto& ecal_sample = ecal_sample_.topic();
+    const std::string& topic_name = ecal_sample.tname();
+    const std::string& topic_id   = ecal_sample.tid();
+    const std::string  process_id = std::to_string(ecal_sample.pid());
+
+    // unregister local subscriber
+    const std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    auto res = m_topic_name_datawriter_map.equal_range(topic_name);
+    for (TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
+    {
+      iter->second->RemoveLocSubscription(process_id, topic_id);
+    }
+  }
+
   void CPubGate::ApplyExtSubRegistration(const eCAL::pb::Sample& ecal_sample_)
   {
     if(!m_created) return;
 
-    const auto& ecal_sample_topic = ecal_sample_.topic();
-    const std::string& host_name  = ecal_sample_topic.hname();
-    const std::string& topic_name = ecal_sample_topic.tname();
-    const std::string& topic_id   = ecal_sample_topic.tid();
-    const std::string& topic_type = ecal_sample_topic.ttype();
-    const std::string& topic_desc = ecal_sample_topic.tdesc();
-    const std::string  process_id = std::to_string(ecal_sample_topic.pid());
+    const auto& ecal_sample = ecal_sample_.topic();
+    const std::string& host_name  = ecal_sample.hname();
+    const std::string& topic_name = ecal_sample.tname();
+    const std::string& topic_id   = ecal_sample.tid();
+    const std::string& topic_type = ecal_sample.ttype();
+    const std::string& topic_desc = ecal_sample.tdesc();
+    const std::string  process_id = std::to_string(ecal_sample.pid());
 
     std::string reader_par;
-    for (const auto& layer : ecal_sample_topic.tlayer())
+    for (const auto& layer : ecal_sample.tlayer())
     {
       // layer parameter as protobuf message
       // this parameter is not used at all currently
@@ -166,11 +182,30 @@ namespace eCAL
     ApplyTopicToDescGate(topic_name, topic_type, topic_desc);
 
     // register external subscriber
-    std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    const std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     auto res = m_topic_name_datawriter_map.equal_range(topic_name);
     for(TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
     {
       iter->second->ApplyExtSubscription(host_name, process_id, topic_id, topic_type, topic_desc, reader_par);
+    }
+  }
+
+  void CPubGate::ApplyExtSubUnregistration(const eCAL::pb::Sample& ecal_sample_)
+  {
+    if (!m_created) return;
+
+    const auto& ecal_sample = ecal_sample_.topic();
+    const std::string& host_name  = ecal_sample.hname();
+    const std::string& topic_name = ecal_sample.tname();
+    const std::string& topic_id   = ecal_sample.tid();
+    const std::string  process_id = std::to_string(ecal_sample.pid());
+
+    // unregister external subscriber
+    const std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    auto res = m_topic_name_datawriter_map.equal_range(topic_name);
+    for (TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
+    {
+      iter->second->RemoveExtSubscription(host_name, process_id, topic_id);
     }
   }
 
@@ -179,7 +214,7 @@ namespace eCAL
     if (!m_created) return;
 
     // refresh publisher registrations
-    std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
+    const std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     for (auto iter : m_topic_name_datawriter_map)
     {
       iter.second->RefreshRegistration();
@@ -188,7 +223,7 @@ namespace eCAL
 
   bool CPubGate::ApplyTopicToDescGate(const std::string& topic_name_, const std::string& topic_type_, const std::string& topic_desc_)
   {
-    if (g_descgate())
+    if (g_descgate() != nullptr)
     {
       // Calculate the quality of the current info
       ::eCAL::CDescGate::QualityFlags quality = ::eCAL::CDescGate::QualityFlags::NO_QUALITY;

ecal/core/src/readwrite/ecal_writer.cpp
@@ -138,7 +138,7 @@ namespace eCAL
     m_use_tdesc = Config::IsTopicDescriptionSharingEnabled();
 
     // register
-    DoRegister(false);
+    Register(false);
 
     // mark as created
     m_created = true;
@@ -206,7 +206,10 @@ namespace eCAL
       m_event_callback_map.clear();
     }
 
-    m_created           = false;
+    // unregister
+    Unregister();
+
+    m_created = false;
 
     return(true);
   }
@@ -222,7 +225,7 @@ namespace eCAL
 #endif
 
     // register it
-    DoRegister(force);
+    Register(force);
 
     return(true);
   }
@@ -238,7 +241,7 @@ namespace eCAL
 #endif
 
     // register it
-    DoRegister(force);
+    Register(force);
 
     return(true);
   }
@@ -256,7 +259,7 @@ namespace eCAL
 #endif
 
     // register it
-    DoRegister(force);
+    Register(force);
 
     return(true);
   }
@@ -273,7 +276,7 @@ namespace eCAL
 #endif
 
     // register it
-    DoRegister(force);
+    Register(force);
 
     return(true);
   }
@@ -408,7 +411,7 @@ namespace eCAL
     if (!CheckWriterModes())
     {
       // incompatible writer configurations
-      return false;
+      return 0;
     }
 
     // get payload buffer size (one time, to avoid multiple computations)
@@ -464,7 +467,7 @@ namespace eCAL
         if (m_writer.shm.PrepareWrite(wattr))
         {
           // register new to update listening subscribers and rematch
-          DoRegister(true);
+          Register(true);
           Process::SleepMS(5);
         }
 
@@ -525,7 +528,7 @@ namespace eCAL
         if (m_writer.inproc.PrepareWrite(wdata))
         {
           // register new to update listening subscribers and rematch
-          DoRegister(true);
+          Register(true);
           Process::SleepMS(5);
         }
 
@@ -582,7 +585,7 @@ namespace eCAL
         if (m_writer.udp_mc.PrepareWrite(wattr))
         {
           // register new to update listening subscribers and rematch
-          DoRegister(true);
+          Register(true);
           Process::SleepMS(5);
         }
 
@@ -654,10 +657,14 @@ namespace eCAL
   void CDataWriter::ApplyLocSubscription(const std::string& process_id_, const std::string& tid_, const std::string& ttype_, const std::string& tdesc_, const std::string& reader_par_)
   {
     Connect(tid_, ttype_, tdesc_);
+
+    // add key to local subscriber map
+    const std::string topic_key = process_id_ + tid_;
     {
       const std::lock_guard<std::mutex> lock(m_sub_map_sync);
-      m_loc_sub_map[process_id_] = true;
+      m_loc_sub_map[topic_key] = true;
     }
+
     m_loc_subscribed = true;
 
     // add a new local subscription
@@ -670,8 +677,15 @@ namespace eCAL
 #endif
   }
 
-  void CDataWriter::RemoveLocSubscription(const std::string& process_id_)
+  void CDataWriter::RemoveLocSubscription(const std::string& process_id_, const std::string& tid_)
   {
+    // remove key from local subscriber map
+    const std::string topic_key = process_id_ + tid_;
+    {
+      const std::lock_guard<std::mutex> lock(m_sub_map_sync);
+      m_loc_sub_map.erase(topic_key);
+    }
+
     // remove a local subscription
     m_writer.udp_mc.RemLocConnection (process_id_);
     m_writer.shm.RemLocConnection    (process_id_);
@@ -685,10 +699,14 @@ namespace eCAL
   void CDataWriter::ApplyExtSubscription(const std::string& host_name_, const std::string& process_id_, const std::string& tid_, const std::string& ttype_, const std::string& tdesc_, const std::string& reader_par_)
   {
     Connect(tid_, ttype_, tdesc_);
+
+    // add key to external subscriber map
+    const std::string topic_key = host_name_ + process_id_ + tid_;
     {
       const std::lock_guard<std::mutex> lock(m_sub_map_sync);
-      m_ext_sub_map[host_name_] = true;
+      m_ext_sub_map[topic_key] = true;
     }
+
     m_ext_subscribed = true;
 
     // add a new external subscription
@@ -701,8 +719,15 @@ namespace eCAL
 #endif
   }
 
-  void CDataWriter::RemoveExtSubscription(const std::string& host_name_, const std::string& process_id_)
+  void CDataWriter::RemoveExtSubscription(const std::string& host_name_, const std::string& process_id_, const std::string& tid_)
   {
+    // remove key from external subscriber map
+    const std::string topic_key = host_name_ + process_id_ + tid_;
+    {
+      const std::lock_guard<std::mutex> lock(m_sub_map_sync);
+      m_ext_sub_map.erase(topic_key);
+    }
+
     // remove external subscription
     m_writer.udp_mc.RemExtConnection (host_name_, process_id_);
     m_writer.shm.RemExtConnection    (host_name_, process_id_);
@@ -739,7 +764,7 @@ namespace eCAL
     }
 
     // register without send
-    DoRegister(false);
+    Register(false);
 
     // check connection timeouts
     const std::shared_ptr<std::list<std::string>> loc_timeouts = std::make_shared<std::list<std::string>>();
@@ -796,7 +821,7 @@ namespace eCAL
     return(out.str());
   }
 
-  bool CDataWriter::DoRegister(bool force_)
+  bool CDataWriter::Register(bool force_)
   {
     if (m_topic_name.empty()) return(false);
 
@@ -924,7 +949,33 @@ namespace eCAL
 
 #ifndef NDEBUG
     // log it
-    Logging::Log(log_level_debug4, m_topic_name + "::CDataWriter::DoRegister");
+    Logging::Log(log_level_debug4, m_topic_name + "::CDataWriter::Register");
+#endif
+    return(true);
+  }
+
+  bool CDataWriter::Unregister()
+  {
+    if (m_topic_name.empty()) return(false);
+
+    // create command parameter
+    eCAL::pb::Sample ecal_unreg_sample;
+    ecal_unreg_sample.set_cmd_type(eCAL::pb::bct_unreg_publisher);
+    auto* ecal_reg_sample_mutable_topic = ecal_unreg_sample.mutable_topic();
+    ecal_reg_sample_mutable_topic->set_hname(m_host_name);
+    ecal_reg_sample_mutable_topic->set_hid(m_host_id);
+    ecal_reg_sample_mutable_topic->set_pname(m_pname);
+    ecal_reg_sample_mutable_topic->set_pid(m_pid);
+    ecal_reg_sample_mutable_topic->set_tname(m_topic_name);
+    ecal_reg_sample_mutable_topic->set_tid(m_topic_id);
+    ecal_reg_sample_mutable_topic->set_uname(Process::GetUnitName());
+
+    // unregister publisher
+    if (g_registration_provider() != nullptr) g_registration_provider()->UnregisterTopic(m_topic_name, m_topic_id, ecal_unreg_sample, true);
+
+#ifndef NDEBUG
+    // log it
+    Logging::Log(log_level_debug4, m_topic_name + "::CDataWriter::UnRegister");
 #endif
     return(true);
   }
@@ -979,10 +1030,10 @@ namespace eCAL
     }
   }
 
-  bool CDataWriter::SetUseUdpMC(TLayer::eSendMode mode_)
+  void CDataWriter::SetUseUdpMC(TLayer::eSendMode mode_)
   {
     m_writer.udp_mc_mode.requested = mode_;
-    if (!m_created) return true;
+    if (!m_created) return;
 
     // log send mode
     LogSendMode(mode_, m_topic_name + "::CDataWriter::Create::UDP_MC_SENDMODE::");
@@ -1001,14 +1052,12 @@ namespace eCAL
       m_writer.udp_mc.Destroy();
       break;
     }
-
-    return(true);
   }
 
-  bool CDataWriter::SetUseShm(TLayer::eSendMode mode_)
+  void CDataWriter::SetUseShm(TLayer::eSendMode mode_)
   {
     m_writer.shm_mode.requested = mode_;
-    if (!m_created) return true;
+    if (!m_created) return;
 
     // log send mode
     LogSendMode(mode_, m_topic_name + "::CDataWriter::Create::SHM_SENDMODE::");
@@ -1027,14 +1076,12 @@ namespace eCAL
       m_writer.shm.Destroy();
       break;
     }
-
-    return(true);
   }
 
-  bool CDataWriter::SetUseTcp(TLayer::eSendMode mode_)
+  void CDataWriter::SetUseTcp(TLayer::eSendMode mode_)
   {
     m_writer.tcp_mode.requested = mode_;
-    if (!m_created) return true;
+    if (!m_created) return;
 
     // log send mode
     LogSendMode(mode_, m_topic_name + "::CDataWriter::Create::TCP_SENDMODE::");
@@ -1053,14 +1100,12 @@ namespace eCAL
       m_writer.tcp.Destroy();
       break;
     }
-
-    return(true);
   }
 
-  bool CDataWriter::SetUseInProc(TLayer::eSendMode mode_)
+  void CDataWriter::SetUseInProc(TLayer::eSendMode mode_)
   {
     m_writer.inproc_mode.requested = mode_;
-    if (!m_created) return true;
+    if (!m_created) return;
 
     // log send mode
     LogSendMode(mode_, m_topic_name + "::CDataWriter::Create::INPROC_SENDMODE::");
@@ -1078,8 +1123,6 @@ namespace eCAL
       m_writer.inproc.Destroy();
       break;
     }
-
-    return(true);
   }
 
   bool CDataWriter::CheckWriterModes()

commit 86613050d0c298a894db5d885b70c397bba13006

Fixes bug regarding unregistration introduced with #1086. (#1197)
Introduces two new structs (SLocalSubscriptionInfo and SExternSubscriptionInfo) and uses them for applying / unapplying registrations.
They are also stored in the ExpiredMap when topic are no longer visible on monitoring layer.

ecal/core/src/pubsub/ecal_pubgate.cpp
@@ -114,9 +114,10 @@ namespace eCAL
 
     const auto& ecal_sample = ecal_sample_.topic();
     const std::string& topic_name = ecal_sample.tname();
-    const std::string& topic_id   = ecal_sample.tid();
+    CDataWriter::SLocalSubscriptionInfo subscription_info;
+    subscription_info.topic_id = ecal_sample.tid();
+    subscription_info.process_id = std::to_string(ecal_sample.pid());
     SDataTypeInformation topic_information{ eCALSampleToTopicInformation(ecal_sample_) };
-    const std::string  process_id = std::to_string(ecal_sample.pid());
 
     std::string reader_par;
     for (const auto& layer : ecal_sample.tlayer())
@@ -135,7 +136,7 @@ namespace eCAL
     auto res = m_topic_name_datawriter_map.equal_range(topic_name);
     for(TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
     {
-      iter->second->ApplyLocSubscription(process_id, topic_id, topic_information, reader_par);
+      iter->second->ApplyLocSubscription(subscription_info, topic_information, reader_par);
     }
   }
 
@@ -145,15 +146,16 @@ namespace eCAL
 
     const auto& ecal_sample = ecal_sample_.topic();
     const std::string& topic_name = ecal_sample.tname();
-    const std::string& topic_id   = ecal_sample.tid();
-    const std::string  process_id = std::to_string(ecal_sample.pid());
+    CDataWriter::SLocalSubscriptionInfo subscription_info;
+    subscription_info.topic_id = ecal_sample.tid();
+    subscription_info.process_id = std::to_string(ecal_sample.pid());
 
     // unregister local subscriber
     const std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     auto res = m_topic_name_datawriter_map.equal_range(topic_name);
     for (TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
     {
-      iter->second->RemoveLocSubscription(process_id, topic_id);
+      iter->second->RemoveLocSubscription(subscription_info);
     }
   }
 
@@ -162,11 +164,12 @@ namespace eCAL
     if(!m_created) return;
 
     const auto& ecal_sample = ecal_sample_.topic();
-    const std::string& host_name  = ecal_sample.hname();
     const std::string& topic_name = ecal_sample.tname();
-    const std::string& topic_id   = ecal_sample.tid();
+    CDataWriter::SExternalSubscriptionInfo subscription_info;
+    subscription_info.host_name  = ecal_sample.hname();
+    subscription_info.topic_id   = ecal_sample.tid();
+    subscription_info.process_id = std::to_string(ecal_sample.pid());
     SDataTypeInformation topic_information{ eCALSampleToTopicInformation(ecal_sample_) };
-    const std::string  process_id = std::to_string(ecal_sample.pid());
 
     std::string reader_par;
     for (const auto& layer : ecal_sample.tlayer())
@@ -185,7 +188,7 @@ namespace eCAL
     auto res = m_topic_name_datawriter_map.equal_range(topic_name);
     for(TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
     {
-      iter->second->ApplyExtSubscription(host_name, process_id, topic_id, topic_information, reader_par);
+      iter->second->ApplyExtSubscription(subscription_info, topic_information, reader_par);
     }
   }
 
@@ -194,17 +197,18 @@ namespace eCAL
     if (!m_created) return;
 
     const auto& ecal_sample = ecal_sample_.topic();
-    const std::string& host_name  = ecal_sample.hname();
     const std::string& topic_name = ecal_sample.tname();
-    const std::string& topic_id   = ecal_sample.tid();
-    const std::string  process_id = std::to_string(ecal_sample.pid());
+    CDataWriter::SExternalSubscriptionInfo subscription_info;
+    subscription_info.host_name = ecal_sample.hname();
+    subscription_info.topic_id = ecal_sample.tid();
+    subscription_info.process_id = std::to_string(ecal_sample.pid());
 
     // unregister external subscriber
     const std::shared_lock<std::shared_timed_mutex> lock(m_topic_name_datawriter_sync);
     auto res = m_topic_name_datawriter_map.equal_range(topic_name);
     for (TopicNameDataWriterMapT::const_iterator iter = res.first; iter != res.second; ++iter)
     {
-      iter->second->RemoveExtSubscription(host_name, process_id, topic_id);
+      iter->second->RemoveExtSubscription(subscription_info);
     }
   }
 

ecal/core/src/readwrite/ecal_writer.cpp
@@ -655,22 +655,21 @@ namespace eCAL
     else         return 0;
   }
 
-  void CDataWriter::ApplyLocSubscription(const std::string& process_id_, const std::string& tid_, const SDataTypeInformation& tinfo_, const std::string& reader_par_)
+  void CDataWriter::ApplyLocSubscription(const SLocalSubscriptionInfo& local_info_, const SDataTypeInformation& tinfo_, const std::string& reader_par_)
   {
-    Connect(tid_, tinfo_);
+    Connect(local_info_.topic_id, tinfo_);
 
     // add key to local subscriber map
-    const std::string topic_key = process_id_ + tid_;
     {
       const std::lock_guard<std::mutex> lock(m_sub_map_sync);
-      m_loc_sub_map[topic_key] = true;
+      m_loc_sub_map[local_info_] = true;
     }
 
     m_loc_subscribed = true;
 
     // add a new local subscription
-    m_writer.udp_mc.AddLocConnection (process_id_, reader_par_);
-    m_writer.shm.AddLocConnection    (process_id_, reader_par_);
+    m_writer.udp_mc.AddLocConnection (local_info_.process_id, reader_par_);
+    m_writer.shm.AddLocConnection    (local_info_.process_id, reader_par_);
 
 #ifndef NDEBUG
     // log it
@@ -678,18 +677,17 @@ namespace eCAL
 #endif
   }
 
-  void CDataWriter::RemoveLocSubscription(const std::string& process_id_, const std::string& tid_)
+  void CDataWriter::RemoveLocSubscription(const SLocalSubscriptionInfo& local_info_)
   {
     // remove key from local subscriber map
-    const std::string topic_key = process_id_ + tid_;
     {
       const std::lock_guard<std::mutex> lock(m_sub_map_sync);
-      m_loc_sub_map.erase(topic_key);
+      m_loc_sub_map.erase(local_info_);
     }
 
     // remove a local subscription
-    m_writer.udp_mc.RemLocConnection (process_id_);
-    m_writer.shm.RemLocConnection    (process_id_);
+    m_writer.udp_mc.RemLocConnection (local_info_.process_id);
+    m_writer.shm.RemLocConnection    (local_info_.process_id);
 
 #ifndef NDEBUG
     // log it
@@ -697,22 +695,21 @@ namespace eCAL
 #endif
   }
 
-  void CDataWriter::ApplyExtSubscription(const std::string& host_name_, const std::string& process_id_, const std::string& tid_, const SDataTypeInformation& tinfo_, const std::string& reader_par_)
+  void CDataWriter::ApplyExtSubscription(const SExternalSubscriptionInfo& external_info_, const SDataTypeInformation& tinfo_, const std::string& reader_par_)
   {
-    Connect(tid_, tinfo_);
+    Connect(external_info_.topic_id, tinfo_);
 
     // add key to external subscriber map
-    const std::string topic_key = host_name_ + process_id_ + tid_;
     {
       const std::lock_guard<std::mutex> lock(m_sub_map_sync);
-      m_ext_sub_map[topic_key] = true;
+      m_ext_sub_map[external_info_] = true;
     }
 
     m_ext_subscribed = true;
 
     // add a new external subscription
-    m_writer.udp_mc.AddExtConnection (host_name_, process_id_, reader_par_);
-    m_writer.shm.AddExtConnection    (host_name_, process_id_, reader_par_);
+    m_writer.udp_mc.AddExtConnection (external_info_.host_name, external_info_.process_id, reader_par_);
+    m_writer.shm.AddExtConnection    (external_info_.host_name, external_info_.process_id, reader_par_);
 
 #ifndef NDEBUG
     // log it
@@ -720,18 +717,17 @@ namespace eCAL
 #endif
   }
 
-  void CDataWriter::RemoveExtSubscription(const std::string& host_name_, const std::string& process_id_, const std::string& tid_)
+  void CDataWriter::RemoveExtSubscription(const SExternalSubscriptionInfo& external_info_)
   {
     // remove key from external subscriber map
-    const std::string topic_key = host_name_ + process_id_ + tid_;
     {
       const std::lock_guard<std::mutex> lock(m_sub_map_sync);
-      m_ext_sub_map.erase(topic_key);
+      m_ext_sub_map.erase(external_info_);
     }
 
     // remove external subscription
-    m_writer.udp_mc.RemExtConnection (host_name_, process_id_);
-    m_writer.shm.RemExtConnection    (host_name_, process_id_);
+    m_writer.udp_mc.RemExtConnection (external_info_.host_name, external_info_.process_id);
+    m_writer.shm.RemExtConnection    (external_info_.host_name, external_info_.process_id);
   }
 
   void CDataWriter::RefreshRegistration()
@@ -768,7 +764,8 @@ namespace eCAL
     Register(false);
 
     // check connection timeouts
-    const std::shared_ptr<std::list<std::string>> loc_timeouts = std::make_shared<std::list<std::string>>();
+    // Todo: Why are only Local connections removed, not external connections?
+    const std::shared_ptr<std::list<SLocalSubscriptionInfo>> loc_timeouts = std::make_shared<std::list<SLocalSubscriptionInfo>>();
     {
       const std::lock_guard<std::mutex> lock(m_sub_map_sync);
       m_loc_sub_map.remove_deprecated(loc_timeouts.get());
@@ -780,7 +777,7 @@ namespace eCAL
 
     for(const auto& loc_sub : *loc_timeouts)
     {
-      m_writer.shm.RemLocConnection(loc_sub);
+      m_writer.shm.RemLocConnection(loc_sub.process_id);
     }
 
     if (!m_loc_subscribed && !m_ext_subscribed)
@@ -1275,7 +1272,7 @@ namespace eCAL
     const std::lock_guard<std::mutex> lock(m_sub_map_sync);
     for (auto sub : m_loc_sub_map)
     {
-      if (sub.first != process_id)
+      if (sub.first.process_id != process_id)
       {
         is_internal_only = false;
         break;

ecal/core/src/readwrite/ecal_writer.h
@@ -47,6 +47,31 @@ namespace eCAL
   class CDataWriter
   {
   public:
+    struct SExternalSubscriptionInfo
+    {
+      std::string host_name;
+      std::string process_id;
+      std::string topic_id;
+
+      friend bool operator<(const SExternalSubscriptionInfo& l, const SExternalSubscriptionInfo& r)
+      {
+        return std::tie(l.host_name, l.process_id, l.topic_id)
+          < std::tie(r.host_name, r.process_id, r.topic_id);
+      }
+    };
+
+    struct SLocalSubscriptionInfo
+    {
+      std::string process_id;
+      std::string topic_id;
+
+      friend bool operator<(const SLocalSubscriptionInfo& l, const SLocalSubscriptionInfo& r)
+      {
+        return std::tie(l.process_id, l.topic_id)
+          < std::tie(r.process_id, r.topic_id);
+      }
+    };
+
     CDataWriter();
     ~CDataWriter();
 
@@ -77,11 +102,11 @@ namespace eCAL
 
     size_t Write(CPayloadWriter& payload_, long long time_, long long id_);
 
-    void ApplyLocSubscription(const std::string& process_id_, const std::string& tid_, const SDataTypeInformation& tinfo_, const std::string& reader_par_);
-    void RemoveLocSubscription(const std::string & process_id_, const std::string& tid_);
+    void ApplyLocSubscription(const SLocalSubscriptionInfo& local_info_, const SDataTypeInformation& tinfo_, const std::string& reader_par_);
+    void RemoveLocSubscription(const SLocalSubscriptionInfo& local_info_);
 
-    void ApplyExtSubscription(const std::string& host_name_, const std::string& process_id_, const std::string& tid_, const SDataTypeInformation& tinfo_, const std::string& reader_par_);
-    void RemoveExtSubscription(const std::string & host_name_, const std::string & process_id_, const std::string& tid_);
+    void ApplyExtSubscription(const SExternalSubscriptionInfo& external_info_, const SDataTypeInformation& tinfo_, const std::string& reader_par_);
+    void RemoveExtSubscription(const SExternalSubscriptionInfo& external_info_);
 
     void RefreshRegistration();
     void RefreshSendCounter();
@@ -137,10 +162,12 @@ namespace eCAL
     std::vector<char>  m_payload_buffer;
 
     std::atomic<bool>  m_connected;
-    using ConnectedMapT = Util::CExpMap<std::string, bool>;
-    mutable std::mutex m_sub_map_sync;
-    ConnectedMapT      m_loc_sub_map;
-    ConnectedMapT      m_ext_sub_map;
+
+    using LocalConnectedMapT = Util::CExpMap<SLocalSubscriptionInfo, bool>;
+    using ExternalConnectedMapT = Util::CExpMap<SExternalSubscriptionInfo, bool>;
+    mutable std::mutex    m_sub_map_sync;
+    LocalConnectedMapT    m_loc_sub_map;
+    ExternalConnectedMapT m_ext_sub_map;
 
     std::mutex         m_event_callback_map_sync;
     using EventCallbackMapT = std::map<eCAL_Publisher_Event, PubEventCallbackT>;