eclipse-ecal/ecal

commit 2333d200221141112fd0eaa924d76b415b89b971

performance: qnx memory allocations

ecal/core/src/io/ecal_memfile_pool.cpp
@@ -174,9 +174,6 @@ namespace eCAL
           }
           else
           {
-            // clear receive buffer
-            m_ecal_buffer.clear();
-
             bool zero_copy_allowed = mfile_hdr.options.zero_copy != 0;
             bool post_process_buffer(false);
             // -------------------------------------------------------------------------

ecal/core/CMakeLists.txt
@@ -190,7 +190,6 @@ set(ecal_custom_tclap_header_src
 set(ecal_io_header_src
     src/io/ecal_memfile_header.h
     src/io/ecal_receiver.h
-    src/io/ecal_sender.h
     src/io/msg_type.h
     src/io/rcv_sample.h
     src/io/snd_raw_buffer.h

ecal/core/src/ecal_log_impl.cpp
@@ -61,19 +61,19 @@
 
 static bool isDirectory(const std::string& path_)
 {
-  if (path_.empty()) return false;
+    if (path_.empty()) return false;
 
-  return EcalUtils::Filesystem::IsDir(path_, EcalUtils::Filesystem::Current);
-}
+    return EcalUtils::Filesystem::IsDir(path_, EcalUtils::Filesystem::Current);
+  }
 
 static std::string get_time_str()
-{
-  auto t = std::time(nullptr);
-  auto tm = *std::localtime(&t);
-  std::stringstream tstream;
-  tstream << std::put_time(&tm, "%Y-%m-%d-%H-%M-%S");
-  return(tstream.str());
-}
+  {
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::stringstream tstream;
+    tstream << std::put_time(&tm, "%Y-%m-%d-%H-%M-%S");
+    return(tstream.str());
+  }
 #endif
 
 #ifdef ECAL_OS_LINUX
@@ -109,7 +109,6 @@ namespace eCAL
 {
   CLog::CLog() :
           m_created(false),
-          m_udp_sender(new CUDPSender()),
           m_pid(0),
           m_logfile(nullptr),
           m_level(log_level_none),
@@ -167,12 +166,12 @@ namespace eCAL
         attr.ipaddr    = Config::GetUdpMulticastGroup();
         attr.broadcast = false;
       }
-      attr.port     = Config::GetUdpMulticastPort() + NET_UDP_MULTICAST_PORT_LOG_OFF;
-      attr.loopback = true;
-      attr.ttl      = Config::GetUdpMulticastTtl();
-      attr.sndbuf   = Config::GetUdpMulticastSndBufSizeBytes();
+      attr.port      = Config::GetUdpMulticastPort() + NET_UDP_MULTICAST_PORT_LOG_OFF;
+      attr.loopback  = true;
+      attr.ttl       = Config::GetUdpMulticastTtl();
+      attr.sndbuf    = Config::GetUdpMulticastSndBufSizeBytes();
 
-      m_udp_sender->Create(attr);
+      m_udp_sender = std::make_unique<CUDPSender>(attr);
     }
 
     m_created = true;
@@ -184,7 +183,7 @@ namespace eCAL
 
     std::lock_guard<std::mutex> lock(m_log_sync);
 
-    m_udp_sender->Destroy();
+    m_udp_sender.reset();
 
     if(m_logfile) fclose(m_logfile);
     m_logfile = nullptr;
@@ -193,13 +192,13 @@ namespace eCAL
   }
 
   void CLog::SetLogLevel(const eCAL_Logging_eLogLevel level_)
-  { 
+  {
     std::lock_guard<std::mutex> lock(m_log_sync);
     m_level = level_;
   };
 
   eCAL_Logging_eLogLevel CLog::GetLogLevel()
-  { 
+  {
     std::lock_guard<std::mutex> lock(m_log_sync);
     return(m_level);
   };

ecal/core/src/ecal_registration_provider.cpp
@@ -48,7 +48,6 @@ namespace eCAL
   std::atomic<bool> CRegistrationProvider::m_created;
 
   CRegistrationProvider::CRegistrationProvider() :
-                    m_multicast_group(NET_UDP_MULTICAST_GROUP),
                     m_reg_refresh(CMN_REGISTRATION_REFRESH),
                     m_reg_topics(false),
                     m_reg_services(false),
@@ -79,27 +78,27 @@ namespace eCAL
 
     if (m_use_network_monitoring)
     {
+      // set network attributes
       SSenderAttr attr;
       bool local_only = !Config::IsNetworkEnabled();
       // for local only communication we switch to local broadcasting to bypass vpn's or firewalls
       if (local_only)
       {
-        attr.ipaddr = "127.255.255.255";
+        attr.ipaddr    = "127.255.255.255";
         attr.broadcast = true;
       }
       else
       {
-        attr.ipaddr = Config::GetUdpMulticastGroup();
+        attr.ipaddr    = Config::GetUdpMulticastGroup();
         attr.broadcast = false;
       }
-      attr.port = Config::GetUdpMulticastPort() + NET_UDP_MULTICAST_PORT_REG_OFF;
+      attr.port     = Config::GetUdpMulticastPort() + NET_UDP_MULTICAST_PORT_REG_OFF;
       attr.loopback = true;
-      attr.ttl = Config::GetUdpMulticastTtl();
-      attr.sndbuf = Config::GetUdpMulticastSndBufSizeBytes();
+      attr.ttl      = Config::GetUdpMulticastTtl();
+      attr.sndbuf   = Config::GetUdpMulticastSndBufSizeBytes();
 
-      m_multicast_group = attr.ipaddr;
-
-      m_reg_snd.Create(attr);
+      // create udp sample sender
+      m_reg_sample_snd = std::make_shared<CSampleSender>(attr);
     }
     else
     {
@@ -115,7 +114,7 @@ namespace eCAL
     }
 #endif
 
-    m_reg_snd_thread.Start(Config::GetRegistrationRefreshMs(), std::bind(&CRegistrationProvider::RegisterSendThread, this));
+    m_reg_sample_snd_thread.Start(Config::GetRegistrationRefreshMs(), std::bind(&CRegistrationProvider::RegisterSendThread, this));
 
     m_created = true;
   }
@@ -124,7 +123,8 @@ namespace eCAL
   {
     if(!m_created) return;
 
-    m_reg_snd_thread.Stop();
+    m_reg_sample_snd.reset();
+    m_reg_sample_snd_thread.Stop();
 
 #ifndef ECAL_LAYER_ICEORYX
     if(m_use_shm_monitoring)
@@ -364,8 +364,8 @@ namespace eCAL
 
     bool return_value {true};
 
-    if(m_use_network_monitoring)
-      return_value &= (SendSample(&m_reg_snd, sample_name_, sample_, m_multicast_group, -1) != 0);
+    if(m_use_network_monitoring && m_reg_sample_snd)
+      return_value &= (m_reg_sample_snd->SendSample(sample_name_, sample_, -1) != 0);
 
 #ifndef ECAL_LAYER_ICEORYX
     if(m_use_shm_monitoring)

ecal/core/src/ecal_registration_provider.h
@@ -29,6 +29,7 @@
 #pragma once
 
 #include "ecal_thread.h"
+#include "io/snd_sample.h"
 #include "io/udp_sender.h"
 
 #ifndef ECAL_LAYER_ICEORYX
@@ -84,36 +85,35 @@ namespace eCAL
     bool SendSampleList(bool reset_sample_list_ = true);
 #endif
 
-    static std::atomic<bool>  m_created;
-    std::string               m_multicast_group;
-    int                       m_reg_refresh;
-    bool                      m_reg_topics;
-    bool                      m_reg_services;
-    bool                      m_reg_process;
+    static std::atomic<bool>          m_created;
+    int                               m_reg_refresh;
+    bool                              m_reg_topics;
+    bool                              m_reg_services;
+    bool                              m_reg_process;
 
-    CUDPSender                m_reg_snd;
-    CThread                   m_reg_snd_thread;
+    CThread                           m_reg_sample_snd_thread;
+    std::shared_ptr<CSampleSender>    m_reg_sample_snd;
 
     typedef std::unordered_map<std::string, eCAL::pb::Sample> SampleMapT;
-    std::mutex                m_topics_map_sync;
-    SampleMapT                m_topics_map;
+    std::mutex                        m_topics_map_sync;
+    SampleMapT                        m_topics_map;
 
-    std::mutex                m_server_map_sync;
-    SampleMapT                m_server_map;
+    std::mutex                        m_server_map_sync;
+    SampleMapT                        m_server_map;
 
-    std::mutex                m_client_map_sync;
-    SampleMapT                m_client_map;
+    std::mutex                        m_client_map_sync;
+    SampleMapT                        m_client_map;
 
 #ifndef ECAL_LAYER_ICEORYX
-    std::mutex                m_sample_list_sync;
-    eCAL::pb::SampleList      m_sample_list;
-    std::string               m_sample_list_buffer;
+    std::mutex                        m_sample_list_sync;
+    eCAL::pb::SampleList              m_sample_list;
+    std::string                       m_sample_list_buffer;
 
-    eCAL::CMemoryFileBroadcast m_memfile_broadcast;
-    eCAL::CMemoryFileBroadcastWriter m_memfile_broadcast_writer;
+    eCAL::CMemoryFileBroadcast        m_memfile_broadcast;
+    eCAL::CMemoryFileBroadcastWriter  m_memfile_broadcast_writer;
 #endif
 
-    bool m_use_network_monitoring;
-    bool m_use_shm_monitoring;
+    bool                              m_use_network_monitoring;
+    bool                              m_use_shm_monitoring;
   };
 };

ecal/core/src/io/ecal_sender.h
@@ -1,85 +1,0 @@
-/* ========================= eCAL LICENSE =================================
- *
- * Copyright (C) 2016 - 2019 Continental Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * ========================= eCAL LICENSE =================================
-*/
-
-/**
- * @brief  eCAL sender base class
-**/
-
-#pragma once
-
-#include <stddef.h>
-#include <string>
-
-namespace eCAL
-{
-  ////////////////////////////////////////////////////////
-  // sender base class
-  ////////////////////////////////////////////////////////
-  struct SSenderAttr
-  {
-    SSenderAttr() :
-      port(0),
-      ttl(0),
-      broadcast(false),
-      unicast(false),
-      loopback(true),
-      sndbuf(1024 * 1024)
-    {};
-
-    std::string ipaddr;
-    int         port;
-    int         ttl;
-    bool        broadcast;
-    bool        unicast;
-    bool        loopback;
-    int         sndbuf;
-  };
-
-  class CSender
-  {
-  public:
-    enum eSocketType
-    {
-      SType_Unknown = 0,
-      SType_SenderUDP,
-    };
-
-    CSender() : m_skt_type(SType_Unknown)
-    {
-    };
-
-    explicit CSender(eSocketType skt_type_) : m_skt_type(skt_type_)
-    {
-    };
-
-    virtual ~CSender()
-    {
-    };
-
-    virtual bool Create(const SSenderAttr& attr_) = 0;
-    virtual bool Destroy() = 0;
-
-    virtual size_t Send(const void* buf_, const size_t len_, const char* ipaddr_ = nullptr) = 0;
-
-    inline eSocketType GetType() const {return m_skt_type;};
-
-  protected:
-    eSocketType m_skt_type;
-  };
-}

ecal/core/src/io/rcv_sample.cpp
@@ -87,7 +87,6 @@ int CReceiveSlot::OnMessageStart(const struct SUDPMessage& ecal_message_)
   m_message_curr_len = 0;
 
   // prepare receive buffer
-  m_recv_buffer.clear();
   m_recv_buffer.reserve(static_cast<size_t>(m_message_total_len));
 
   // switch to reading mode

ecal/core/src/io/snd_sample.cpp
@@ -29,7 +29,7 @@
 
 namespace
 {
-  size_t TransmitToUDP(const void* buf_, const size_t len_, eCAL::CUDPSender* sample_sender_, const std::string& mcast_address_)
+  size_t TransmitToUDP(const void* buf_, const size_t len_, const std::shared_ptr<eCAL::CUDPSender>& sample_sender_, const std::string& mcast_address_)
   {
     return (sample_sender_->Send(buf_, len_, mcast_address_.c_str()));
   }
@@ -37,19 +37,23 @@ namespace
 
 namespace eCAL
 {
-  size_t SendSample(eCAL::CUDPSender* udp_sender_, const std::string& sample_name_, const eCAL::pb::Sample& ecal_sample_, const std::string& ipaddr_, long bandwidth_)
+  CSampleSender::CSampleSender(const SSenderAttr& attr_)
   {
-    if (udp_sender_ == nullptr) return(0);
+    m_udp_sender = std::make_shared<CUDPSender>(attr_);
+  }
+
+  size_t CSampleSender::SendSample(const std::string& sample_name_, const eCAL::pb::Sample& ecal_sample_, long bandwidth_)
+  {
+    if (m_udp_sender == nullptr) return(0);
 
     // return value
     size_t sent_sum(0);
 
-    std::vector<char> payload_sum;
-    size_t data_size = CreateSampleBuffer(sample_name_, ecal_sample_, payload_sum);
+    const size_t data_size = CreateSampleBuffer(sample_name_, ecal_sample_, m_payload);
     if (data_size > 0)
     {
       // and send it
-      sent_sum = SendSampleBuffer(payload_sum.data(), data_size, bandwidth_, std::bind(TransmitToUDP, std::placeholders::_1, std::placeholders::_2, udp_sender_, ipaddr_));
+      sent_sum = SendSampleBuffer(m_payload.data(), data_size, bandwidth_, std::bind(TransmitToUDP, std::placeholders::_1, std::placeholders::_2, m_udp_sender, m_attr.ipaddr));
 
 #ifndef NDEBUG
       // log it

commit 3529ee2e487797c46a8700c680045548dc424da4

2333d20 introduced a bug when sending zero length messages over SHM - the callback would contain stale non-zero length messages from the previous call. This commit fixes the introduced bug, and adds a testcase for this scenario.

ecal/core/src/io/ecal_memfile_pool.cpp
@@ -206,19 +206,18 @@ namespace eCAL
             // and close the file immediately
             else
             {
+              // need to resize the buffer especially if data_size = 0, otherwise it might contain stale data.
+              receive_buffer.resize((size_t)mfile_hdr.data_size);
+
               // read payload
               // if data length == 0, there is no need to further read data
               // we just flag to process the empty buffer
-              if (mfile_hdr.data_size == 0)
+              if (mfile_hdr.data_size != 0)
               {
-                post_process_buffer = true;
-              }
-              else
-              {
-                receive_buffer.resize((size_t)mfile_hdr.data_size);
                 m_memfile.Read(receive_buffer.data(), (size_t)mfile_hdr.data_size, mfile_hdr.hdr_size);
-                post_process_buffer = true;
               }
+
+              post_process_buffer = true;
             }
 
             // store clock

testing/ecal/pubsub_test/src/pubsub_test.cpp
@@ -18,6 +18,8 @@
 */
 
 #include <ecal/ecal.h>
+#include <ecal/msg/string/publisher.h>
+#include <ecal/msg/string/subscriber.h>
 
 #include <atomic>
 #include <thread>
@@ -760,6 +762,116 @@ TEST(IO, ZeroPayloadMessageUDP)
   // finalize eCAL API
   eCAL::Finalize();
 }
+
+
+TEST(IO, MultipleSendsSHM)
+{
+  // default send string
+  std::vector<std::string> send_vector{ "this", "is", "a", "", "testtest" };
+  std::string last_received_msg;
+  long long   last_received_timestamp;
+
+  // initialize eCAL API
+  eCAL::Initialize(0, nullptr, "pubsub_test");
+
+  // publish / subscribe match in the same process
+  eCAL::Util::EnableLoopback(true);
+
+  // create subscriber for topic "A"
+  eCAL::string::CSubscriber<std::string> sub("A");
+
+  // create publisher for topic "A"
+  eCAL::string::CPublisher<std::string> pub("A");
+  pub.SetLayerMode(eCAL::TLayer::tlayer_all, eCAL::TLayer::smode_off);
+  pub.SetLayerMode(eCAL::TLayer::tlayer_shm, eCAL::TLayer::smode_on);
+  pub.ShmSetAcknowledgeTimeout(10); // Make sure we receive the data
+
+  // add callback
+  auto save_data = [&last_received_msg, &last_received_timestamp](const char* /*topic_name_*/, const std::string& msg_, long long time_, long long /*clock_*/, long long /*id_*/)
+  {
+    last_received_msg = msg_;
+    last_received_timestamp = time_;
+  };
+  EXPECT_TRUE(sub.AddReceiveCallback(save_data));
+
+  // let's match them
+  eCAL::Process::SleepMS(2 * CMN_REGISTRATION_REFRESH);
+  long long timestamp = 1;
+  for (const auto elem : send_vector)
+  {
+    pub.Send(elem, timestamp);
+    eCAL::Process::SleepMS(DATA_FLOW_TIME);
+    EXPECT_EQ(last_received_msg, elem);
+    EXPECT_EQ(last_received_timestamp, timestamp);
+    ++timestamp;
+  }
+
+  // destroy subscriber
+  sub.Destroy();
+
+  // destroy publisher
+  pub.Destroy();
+
+  // finalize eCAL API
+  eCAL::Finalize();
+}
+
+
+TEST(IO, MultipleSendsUDP)
+{
+  // default send string
+  std::vector<std::string> send_vector{ "this", "is", "a", "", "testtest" };
+  std::string last_received_msg;
+  long long   last_received_timestamp;
+
+  // initialize eCAL API
+  eCAL::Initialize(0, nullptr, "pubsub_test");
+
+  // publish / subscribe match in the same process
+  eCAL::Util::EnableLoopback(true);
+
+  // create subscriber for topic "A"
+  eCAL::string::CSubscriber<std::string> sub("A");
+
+  // create publisher for topic "A"
+  eCAL::string::CPublisher<std::string> pub("A");
+  pub.SetLayerMode(eCAL::TLayer::tlayer_all, eCAL::TLayer::smode_off);
+  pub.SetLayerMode(eCAL::TLayer::tlayer_udp_mc, eCAL::TLayer::smode_on);
+  pub.ShmSetAcknowledgeTimeout(10); // Make sure we receive the data
+
+  // add callback
+  auto save_data = [&last_received_msg, &last_received_timestamp](const char* /*topic_name_*/, const std::string& msg_, long long time_, long long /*clock_*/, long long /*id_*/)
+  {
+    last_received_msg = msg_;
+    last_received_timestamp = time_;
+  };
+  EXPECT_TRUE(sub.AddReceiveCallback(save_data));
+
+  // let's match them
+  eCAL::Process::SleepMS(2 * CMN_REGISTRATION_REFRESH);
+  long long timestamp = 1;
+  for (const auto elem : send_vector)
+  {
+    pub.Send(elem, timestamp);
+    eCAL::Process::SleepMS(DATA_FLOW_TIME);
+    EXPECT_EQ(last_received_msg, elem);
+    EXPECT_EQ(last_received_timestamp, timestamp);
+    ++timestamp;
+  }
+
+  // destroy subscriber
+  sub.Destroy();
+
+  // destroy publisher
+  pub.Destroy();
+
+  // finalize eCAL API
+  eCAL::Finalize();
+}
+
+
+
+
 
 #if 0
 TEST(IO, ZeroPayloadMessageTCP)