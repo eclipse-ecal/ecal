d03e0f64444f07337485aac920b21256b4d4d46a
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2023-10-09 13:30:02 +0200

    HDF5: Make sure datasets are always closed upon reading, even when size 0 data is read. HDF5MeasDir should return if all files were closed properly.

--- contrib/ecalhdf5/src/eh5_meas_file_v2.cpp
+++ contrib/ecalhdf5/src/eh5_meas_file_v2.cpp
@@ -294,13 +294,15 @@ bool eCAL::eh5::HDF5MeasFileV2::GetEntryData(long long entry_id, void* data) con
 
   auto size = H5Dget_storage_size(dataset_id);
 
-  if (size <= 0) return false;
-
-  auto readStatus = H5Dread(dataset_id, H5T_NATIVE_UCHAR, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+  herr_t read_status = -1;
+  if (size >= 0)
+  {
+    read_status = H5Dread(dataset_id, H5T_NATIVE_UCHAR, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+  }
 
   H5Dclose(dataset_id);
 
-  return (readStatus >= 0);
+  return (read_status >= 0);
 }
 
 
