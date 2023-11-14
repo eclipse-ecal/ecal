0205ec01153c0e4d604e9465add01e34a713c6b3
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2023-11-14 17:52:05 +0100

    change directory for generated ecal_config.

--- thirdparty/asio/build-asio.cmake
+++ thirdparty/asio/build-asio.cmake
@@ -1,5 +1,5 @@
 add_library(asio::asio INTERFACE IMPORTED)
-target_include_directories(asio::asio INTERFACE thirdparty/asio/asio/asio/include)
+target_include_directories(asio::asio INTERFACE ${CMAKE_CURRENT_LIST_DIR}/asio/asio/include)
 target_compile_definitions(asio::asio INTERFACE ASIO_STANDALONE)
 
 list(PREPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/Modules)
