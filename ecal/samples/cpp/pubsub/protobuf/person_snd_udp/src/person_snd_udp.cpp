82c4269a69987c8c1cdc2543c3df76c88ff14d5a
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2024-04-26 08:46:25 +0200

    person snd udp sample added

---
+++ ecal/samples/cpp/pubsub/protobuf/person_snd_udp/src/person_snd_udp.cpp
@@ -0,0 +1,79 @@
+/* ========================= eCAL LICENSE =================================
+ *
+ * Copyright (C) 2016 - 2019 Continental Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================= eCAL LICENSE =================================
+*/
+
+#include <ecal/ecal.h>
+#include <ecal/msg/protobuf/publisher.h>
+
+#include <iostream>
+
+#include "person.pb.h"
+
+int main(int argc, char **argv)
+{
+  // initialize eCAL API
+  eCAL::Initialize(argc, argv, "person publisher multicast");
+
+  // set process state
+  eCAL::Process::SetState(proc_sev_healthy, proc_sev_level1, "I feel good !");
+
+  // create a publisher (topic name "person")
+  eCAL::protobuf::CPublisher<pb::People::Person> pub("person");
+
+  // switch all layer off
+  pub.SetLayerMode(eCAL::TLayer::tlayer_all, eCAL::TLayer::smode_off);
+
+  // switch unicast layer on
+  pub.SetLayerMode(eCAL::TLayer::tlayer_udp_mc, eCAL::TLayer::smode_on);
+
+  // generate a class instance of Person
+  pb::People::Person person;
+
+  // enter main loop
+  auto cnt(0);
+  while (eCAL::Ok())
+  {
+    // set person object content
+    person.set_id(++cnt);
+    person.set_name("Max");
+    person.set_stype(pb::People::Person_SType_MALE);
+    person.set_email("max@mail.net");
+    person.mutable_dog()->set_name("Brandy");
+    person.mutable_house()->set_rooms(4);
+
+    // send the person object
+    pub.Send(person);
+
+    // print content
+    std::cout << "person id    : " << person.id()            << std::endl;
+    std::cout << "person name  : " << person.name()          << std::endl;
+    std::cout << "person stype : " << person.stype()         << std::endl;
+    std::cout << "person email : " << person.email()         << std::endl;
+    std::cout << "dog.name     : " << person.dog().name()    << std::endl;
+    std::cout << "house.rooms  : " << person.house().rooms() << std::endl;
+    std::cout                                                << std::endl;
+
+    // sleep 500 ms
+    eCAL::Process::SleepMS(500);
+  }
+
+  // finalize eCAL API
+  eCAL::Finalize();
+  
+  return(0);
+}
