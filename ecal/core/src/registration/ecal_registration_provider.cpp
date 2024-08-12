1eefe6321d69d5f49f406f852a565d5b63e41e1a
Peguen <73380451+Peguen@users.noreply.github.com>
2024-08-12 14:57:35 +0200

    ecal_registration_provider use registration_refresh from m_config.

--- ecal/core/src/registration/ecal_registration_provider.cpp
+++ ecal/core/src/registration/ecal_registration_provider.cpp
@@ -110,7 +110,7 @@ namespace eCAL
 
     // start cyclic registration thread
     m_reg_sample_snd_thread = std::make_shared<CCallbackThread>(std::bind(&CRegistrationProvider::RegisterSendThread, this));
-    m_reg_sample_snd_thread->start(std::chrono::milliseconds(Config::GetRegistrationRefreshMs()));
+    m_reg_sample_snd_thread->start(std::chrono::milliseconds(m_config.registration_refresh));
 
     m_created = true;
   }
