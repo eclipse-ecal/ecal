be1a9dc4efc7f5bdc01061d6fdcfb01c5df338ef
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2024-12-13 13:43:17 +0100

    proto pub test fixed removed test added again

---
+++ ecal/tests/cpp/pubsub_proto_test/src/proto_publisher_test.cpp
@@ -0,0 +1,72 @@
+/* ========================= eCAL LICENSE =================================
+ *
+ * Copyright (C) 2016 - 2024 Continental Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================= eCAL LICENSE =================================
+*/
+
+// std headers
+// used libraries
+#include <gtest/gtest.h>
+// own project
+#include <ecal/ecal.h>
+#include <ecal/msg/protobuf/publisher.h>
+
+#include <person.pb.h>
+#include <utility>
+
+class ProtoPublisherTest : public ::testing::Test {
+public:
+
+  ProtoPublisherTest() {
+    eCAL::Initialize();
+  }
+
+  ~ProtoPublisherTest() override {
+    eCAL::Finalize();
+  }
+
+};
+using core_cpp_pubsub_proto_pub = ProtoPublisherTest;
+
+TEST_F(core_cpp_pubsub_proto_pub, ProtoPublisherTest_MoveAssignment)
+{
+  eCAL::protobuf::CPublisher<pb::People::Person> person_pub("ProtoPublisherTest");
+
+  ASSERT_TRUE(person_pub.IsCreated());
+
+  eCAL::protobuf::CPublisher<pb::People::Person>person_moved;
+
+  ASSERT_FALSE(person_moved.IsCreated());
+
+  person_moved = std::move(person_pub);
+
+  // New Subscriber must be initialized
+  ASSERT_TRUE(person_moved.IsCreated());
+  // Old subscriber is not initialized
+  ASSERT_FALSE(person_pub.IsCreated());
+}
+
+TEST_F(core_cpp_pubsub_proto_pub, ProtoPublisherTest_MoveConstruction)
+{
+  eCAL::protobuf::CPublisher<pb::People::Person> person_pub("ProtoPublisherTest");
+
+  ASSERT_TRUE(person_pub.IsCreated());
+
+  eCAL::protobuf::CPublisher<pb::People::Person>person_moved{ std::move(person_pub) };
+
+  ASSERT_TRUE(person_moved.IsCreated());
+  ASSERT_FALSE(person_pub.IsCreated());
+}
