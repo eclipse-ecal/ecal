23e7f634746f2bab85a17d8bc3dcfa69a75eaef6
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2024-12-16 16:35:35 +0100

    minor logging fix

--- ecal/core/src/pubsub/ecal_publisher.cpp
+++ ecal/core/src/pubsub/ecal_publisher.cpp
@@ -63,7 +63,7 @@ namespace eCAL
     if (g_pubgate() != nullptr) g_pubgate()->Unregister(m_publisher_impl->GetTopicName(), m_publisher_impl);
 
 #ifndef NDEBUG
-    eCAL::Logging::Log(log_level_debug1, std::string(m_publisher_impl->GetTopicName() + "::CPublisher::Destroy"));
+    eCAL::Logging::Log(log_level_debug1, std::string(m_publisher_impl->GetTopicName() + "::CPublisher::Destructor"));
 #endif
   }
 
