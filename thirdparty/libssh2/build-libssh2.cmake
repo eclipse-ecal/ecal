315df75e3015d1d088bcbfa3e15077c28a024a26
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2024-12-17 11:19:54 +0100

    [submodules] Removed libssh submodule (#1817)

--- thirdparty/libssh2/build-libssh2.cmake
+++
@@ -1,6 +0,0 @@
-include_guard(GLOBAL)
-
-set(DBUILD_STATIC_LIBS OFF CACHE BOOL "My option" FORCE)
-set(BUILD_TESTING OFF CACHE BOOL "My option" FORCE)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/fineftp-server/libssh2 thirdparty/libssh2 EXCLUDE_FROM_ALL)
-add_library(LibSSH2 ALIAS libssh2)
