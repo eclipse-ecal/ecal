af70650fd41a37ee44a9c7024bb3c5bd31a7c4f2
Peguen <73380451+Peguen@users.noreply.github.com>
2025-01-07 14:09:07 +0100

    linuxptp adaption to new api.

--- contrib/ecaltime/linuxptp/src/config/config.h
+++ contrib/ecaltime/linuxptp/src/config/config.h
@@ -1,6 +1,6 @@
 /* ========================= eCAL LICENSE =================================
  *
- * Copyright (C) 2016 - 2019 Continental Corporation
+ * Copyright (C) 2016 - 2025 Continental Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ namespace LinuxPtpConfig {
    * @return the device value from the linuxptp section
    */
   std::string getDevice() {    
-    std::string path_to_ini = eCAL::Util::GeteCALConfigDir();
+    std::string path_to_ini = eCAL::Util::GeteCALDataDir();
     path_to_ini += "ecaltime.yaml";
 
     YAML::Node yaml;
