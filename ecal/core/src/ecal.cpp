7a18429d9974541f105af93dc024e6bf2247af42
Peguen <73380451+Peguen@users.noreply.github.com>
2025-01-09 18:36:28 +0100

    [core] Correctly initialize config when not provided in `Initialize`. (#1882)

--- ecal/core/src/ecal.cpp
+++ ecal/core/src/ecal.cpp
@@ -1,6 +1,6 @@
 /* ========================= eCAL LICENSE =================================
  *
- * Copyright (C) 2016 - 2024 Continental Corporation
+ * Copyright (C) 2016 - 2025 Continental Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,6 +90,7 @@ namespace eCAL
   bool Initialize(const std::string& unit_name_ /*= ""*/, unsigned int components_ /*= Init::Default*/)
   {
     eCAL::Configuration config;
+    config.InitFromConfig();
 
     return Initialize(config, unit_name_, components_);
   }
