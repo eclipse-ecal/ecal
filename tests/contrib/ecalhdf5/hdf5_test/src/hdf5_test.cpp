8e4d7f9a7122b46994d8fc3143216abb0dc08202
KerstinKeller <KerstinKeller@users.noreply.github.com>
2025-01-10 09:28:40 +0100

    [measurement] Use unsigned type for topic id in hdf5 (#1883)

--- tests/contrib/ecalhdf5/hdf5_test/src/hdf5_test.cpp
+++ tests/contrib/ecalhdf5/hdf5_test/src/hdf5_test.cpp
@@ -1,6 +1,6 @@
 /* ========================= eCAL LICENSE =================================
  *
- * Copyright (C) 2016 - 2024 Continental Corporation
+ * Copyright (C) 2016 - 2025 Continental Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1032,7 +1032,6 @@ TEST(HDF5, PrintParseHex)
   std::vector<Channel::id_t> numeric_values =
   {
     0,
-    -1,
     1,
     std::numeric_limits<Channel::id_t>::min(),
     std::numeric_limits<Channel::id_t>::max()
