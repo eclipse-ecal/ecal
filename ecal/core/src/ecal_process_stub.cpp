43896fca32ee0ad20ebf2888ef94cb3d291713ab
DownerCase <119755054+DownerCase@users.noreply.github.com>
2025-01-13 17:07:09 +0000

    Fixup build for Alpine (#1890)

--- ecal/core/src/ecal_process_stub.cpp
+++ ecal/core/src/ecal_process_stub.cpp
@@ -1,6 +1,6 @@
 /* ========================= eCAL LICENSE =================================
  *
- * Copyright (C) 2016 - 2019 Continental Corporation
+ * Copyright (C) 2016 - 2025 Continental Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,7 @@
  * ========================= eCAL LICENSE =================================
 */
 
-#include <cstdlib>
-#include <cerrno>
+#include <errno.h>
 #include <fcntl.h>
 #include <string>
 #include <sys/file.h>
