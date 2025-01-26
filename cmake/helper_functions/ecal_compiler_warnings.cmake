ad7ad752d474dcce8425c421da11fb85976e05eb
DownerCase <downercase8@gmail.com>
2025-01-26 11:07:21 +0000

    Only apply c++ flags to c++ targets

--- cmake/helper_functions/ecal_compiler_warnings.cmake
+++ cmake/helper_functions/ecal_compiler_warnings.cmake
@@ -2,20 +2,26 @@ include_guard(GLOBAL)
 
 add_library(_ecal_warnings INTERFACE)
 
-set(compiler_flags "")
+set(cxx_compiler_flags "")
+
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
   message(STATUS "MSVC detected - Adding flags")
-  set(compiler_flags "/MP" "/W4")
+  set(cxx_compiler_flags "/MP" "/W4")
 elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU|Clang")
   message(STATUS "Setting GNU/Clang flags")
-  set(compiler_flags "-Wall" "-Wextra")
+  set(cxx_compiler_flags "-Wall" "-Wextra")
 else()
   message(WARNING "Unknown compiler, will not set warning flags")
 endif()
-target_compile_options(_ecal_warnings INTERFACE "${compiler_flags}")
 
-unset(compiler_flags)
+target_compile_options(_ecal_warnings INTERFACE
+  "$<$<COMPILE_LANGUAGE:C,CXX>:${cxx_compiler_flags}>"
+)
+
+unset(cxx_compiler_flags)
 
 function(ecal_add_compiler_warnings TARGET_NAME)
-  target_link_libraries("${TARGET_NAME}" PRIVATE $<BUILD_INTERFACE:_ecal_warnings>)
+  target_link_libraries("${TARGET_NAME}" PRIVATE
+    "$<BUILD_INTERFACE:_ecal_warnings>"
+  )
 endfunction()
