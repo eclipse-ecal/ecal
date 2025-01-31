5d28d1f68bcd3e3fd6ae277909129ad3018d28b0
Peguen <peter.guenschmann@gmail.com>
2025-01-31 11:55:56 +0100

    MMA init condition change.

--- contrib/mma/src/mma_application.cpp
+++ contrib/mma/src/mma_application.cpp
@@ -135,7 +135,7 @@ int main(int argc, char** argv)
   std::cout << app_version_header << std::endl << ecal_version_header << std::endl << std::endl;
 
   // initialize eCAL API
-  if (eCAL::Initialize(MMA_APPLICATION_NAME, eCAL::Init::Publisher))
+  if (!eCAL::Initialize(MMA_APPLICATION_NAME, eCAL::Init::Publisher))
   {
     std::cout << "eCAL initialization failed !";
     return 1;
