c6968de23a190eaa63e1ab6a72cc8f2d72b6bee1
DownerCase <downercase8@gmail.com>
2025-02-12 11:58:14 +0000

    Bump tcp_pubsub submodule to get TCP_PUBSUB_LIBRARY_TYPE

--- thirdparty/tcp_pubsub/build-tcp_pubsub.cmake
+++ thirdparty/tcp_pubsub/build-tcp_pubsub.cmake
@@ -1,13 +1,8 @@
 include_guard(GLOBAL)
 
 # Build as static library
-set(BUILD_SHARED_LIBS_OLD ${BUILD_SHARED_LIBS})
-set(BUILD_SHARED_LIBS OFF)
+set(TCP_PUBSUB_LIBRARY_TYPE STATIC)
 
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/tcp_pubsub/tcp_pubsub thirdparty/tcp_pubsub EXCLUDE_FROM_ALL SYSTEM)
-
-# Reset static / shared libs to old value
-set(BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS_OLD})
-unset(BUILD_SHARED_LIBS_OLD)
 
 set_property(TARGET tcp_pubsub PROPERTY FOLDER thirdparty/tcp_pubsub)
