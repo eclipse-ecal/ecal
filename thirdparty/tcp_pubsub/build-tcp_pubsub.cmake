c18fd667dac2ed2bda88bc301a31513ff4afbda2
DownerCase <119755054+DownerCase@users.noreply.github.com>
2025-02-13 06:30:43 +0000

    [cmake]: Fix usage after install when built with `BUILD_SHARED_LIBS=ON` (#2016)
    * Force tcp_pubsub submodule to build static
    
    * Allow ecal-utils to be static or shared
    
    ecal-utils is used by other exported libraries (eg: ecalhdf5) which may
    be static or shared so there is no need to constrain ecal-utils, it
    should be consistent with other libraries
    
    * Force ThreadingUtils to be static implementation
    
    ThreadingUtils is only used internally by applications so it can be static and
    not exported/installed
    
    * Force EcalParser and QEcalParser to be static implementation libraries
    
    Both are only used by executables and aren't exported so they can be
    forced static
    
    * Force CustomQt to be static implementation library
    
    It is only used in applications and monitor plugins so it can be static
    
    * Bump tcp_pubsub submodule to get TCP_PUBSUB_LIBRARY_TYPE
    
    * Fail installer signing gracefully

--- thirdparty/tcp_pubsub/build-tcp_pubsub.cmake
+++ thirdparty/tcp_pubsub/build-tcp_pubsub.cmake
@@ -1,5 +1,11 @@
 include_guard(GLOBAL)
 
+# Build as static library
+set(TCP_PUBSUB_LIBRARY_TYPE STATIC)
+set(TCP_PUBSUB_USE_BUILTIN_ASIO OFF)
+set(TCP_PUBSUB_USE_BUILTIN_RECYCLE OFF)
+set(TCP_PUBSUB_BUILD_TESTS OFF)
+
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/tcp_pubsub/tcp_pubsub thirdparty/tcp_pubsub EXCLUDE_FROM_ALL SYSTEM)
 
 set_property(TARGET tcp_pubsub PROPERTY FOLDER thirdparty/tcp_pubsub)
