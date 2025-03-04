a0c55f2a3c9c5ddecbaf9e8de3b9ab4a9ffcf8a4
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2025-03-04 13:57:30 +0100

    pubsub event types added

--- lang/csharp/Continental.eCAL.Core/ecal_clr_types_pubsub.h
+++ lang/csharp/Continental.eCAL.Core/ecal_clr_types_pubsub.h
@@ -26,12 +26,14 @@
  *
  * This file contains managed wrappers for native eCAL pub/sub types, including
  * the topic identifier (TopicId) and the receive callback data (ReceiveCallbackData),
- * as well as the delegate definition for subscriber receive callbacks.
+ * as well as the delegate definition for subscriber receive callbacks and definitions
+ * for publisher/subscriber events.
  */
 
 #include "ecal_clr_types.h"
 
 using namespace System;
+using namespace System::Collections::Generic;
 
 namespace Continental {
   namespace eCAL {
@@ -104,6 +106,84 @@ namespace Continental {
        * @param data Managed wrapper for the received callback data.
        */
       public delegate void ReceiveCallbackDelegate(TopicId^ publisherId, DataTypeInformation^ dataTypeInfo, ReceiveCallbackData^ data);
+
+      /**
+       * @brief Managed enumeration for publisher events.
+       */
+      public enum class PublisherEvent {
+        None = 0,         /**< No event. */
+        Connected = 1,    /**< A new subscriber has been connected to the publisher. */
+        Disconnected = 2, /**< A previously connected subscriber has been disconnected from the publisher. */
+        Dropped = 3       /**< A subscriber has missed a message. */
+      };
+
+      /**
+       * @brief Managed enumeration for subscriber events.
+       */
+      public enum class SubscriberEvent {
+        None = 0,         /**< No event. */
+        Connected = 1,    /**< A new publisher has been connected to the subscriber. */
+        Disconnected = 2, /**< A previously connected publisher has been disconnected from the subscriber. */
+        Dropped = 3       /**< A message from a publisher has been dropped. */
+      };
+
+      /**
+       * @brief Managed wrapper for the native SPubEventCallbackData structure.
+       *
+       * This class encapsulates publisher event callback information.
+       */
+      public ref class PubEventCallbackData {
+      public:
+        /**
+         * @brief Gets or sets the publisher event type.
+         */
+        property PublisherEvent EventType;
+        /**
+         * @brief Gets or sets the event time in microseconds.
+         */
+        property long long EventTime;
+        /**
+         * @brief Gets or sets the subscriber's data type information.
+         */
+        property DataTypeInformation^ SubscriberDataType;
+      };
+
+      /**
+       * @brief Delegate for publisher event callbacks.
+       *
+       * @param topicId Managed wrapper for the topic identifier.
+       * @param data Managed wrapper for the publisher event callback data.
+       */
+      public delegate void PublisherEventCallbackDelegate(TopicId^ topicId, PubEventCallbackData^ data);
+
+      /**
+       * @brief Managed wrapper for the native SSubEventCallbackData structure.
+       *
+       * This class encapsulates subscriber event callback information.
+       */
+      public ref class SubEventCallbackData {
+      public:
+        /**
+         * @brief Gets or sets the subscriber event type.
+         */
+        property SubscriberEvent EventType;
+        /**
+         * @brief Gets or sets the event time in microseconds.
+         */
+        property long long EventTime;
+        /**
+         * @brief Gets or sets the publisher's data type information.
+         */
+        property DataTypeInformation^ PublisherDataType;
+      };
+
+      /**
+       * @brief Delegate for subscriber event callbacks.
+       *
+       * @param topicId Managed wrapper for the topic identifier.
+       * @param data Managed wrapper for the subscriber event callback data.
+       */
+      public delegate void SubscriberEventCallbackDelegate(TopicId^ topicId, SubEventCallbackData^ data);
 
     } // namespace Core
   } // namespace eCAL
