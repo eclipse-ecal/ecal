5859ba612d2b6ae6ba449ddb81452de731a4d3a4
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2025-03-28 09:26:46 +0100

    [dependencies] Add support for ftxui 6.0.0. Todo: It seems that 6.0.0 breaks the ability to select an item from the list of topics. This needs to be investigated.

--- app/mon/mon_tui/src/tui/view/component/decorator.hpp
+++ app/mon/mon_tui/src/tui/view/component/decorator.hpp
@@ -1,6 +1,6 @@
 /* ========================= eCAL LICENSE =================================
  *
- * Copyright (C) 2016 - 2019 Continental Corporation
+ * Copyright (C) 2016 - 2025 Continental Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,7 +51,11 @@ public:
     Add(child);
   }
 
+#if FTXUI_VERSION_MAJOR >= 6
+  Element OnRender() override
+#else
   Element Render() override
+#endif
   {
     return hbox({
       text("  "),
