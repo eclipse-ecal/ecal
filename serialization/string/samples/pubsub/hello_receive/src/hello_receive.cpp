adbc878fc3b52f5959b17e73f733a10a54d03ead
Peguen <peter.guenschmann@gmail.com>
2025-04-01 15:49:59 +0200

    Adapted c++ hello world examples.

--- serialization/string/samples/pubsub/hello_receive/src/hello_receive.cpp
+++ serialization/string/samples/pubsub/hello_receive/src/hello_receive.cpp
@@ -17,7 +17,12 @@
  * ========================= eCAL LICENSE =================================
 */
 
+// Include the basic eCAL header
 #include <ecal/ecal.h>
+/* 
+  We want to receive raw strings, so wie include the string subscriber header.
+  eCAL supports multiple message formats.
+*/
 #include <ecal/msg/string/subscriber.h>
 
 #include <iostream>
@@ -28,23 +33,42 @@
 int main()
 {
   std::cout << "-------------------------------" << std::endl;
-  std::cout << " HELLO WORLD RECEIVER"           << std::endl;
+  std::cout << " C++: HELLO WORLD RECEIVER"      << std::endl;
   std::cout << "-------------------------------" << std::endl;
 
-  // initialize eCAL API
+  /* 
+    Initialize eCAL. You always have to initialize eCAL beforee using its API.
+    The name of our eCAL Process will be "hello_receive". 
+    This name will be visible in the eCAL Monitor, once the process is running.
+  */
   eCAL::Initialize("hello_receive");
 
-  // subscriber for topic "Hello"
+  /*
+    Creating the eCAL Subscriber. An eCAL Process can create multiple subscribers (and publishers).
+    The topic we are going to receive is called "hello".
+  */
   eCAL::string::CSubscriber sub("hello");
 
-  // receive updates in a callback (lambda function)
+  /*
+    Creating a receive callback. The callback will be called whenever a new message is received.
+  */
   auto msg_cb = [](const std::string& msg_) { std::cout << "Received \"" << msg_ << "\"" << std::endl; };
+  
+  /*
+    Register the callback with the subscriber so it can be called.
+  */
   sub.SetReceiveCallback(std::bind(msg_cb, std::placeholders::_2));
 
-  // idle main loop
+  /*
+    Creating an infinite loop.
+    eCAL Supports a stop signal; when an eCAL Process is stopped, eCAL::Ok() will return false.
+  */
   while (eCAL::Ok()) std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-  // finalize eCAL API
+  /*
+    Deinitialize eCAL.
+    You should always do that before your application exits.
+  */
   eCAL::Finalize();
 
   return(0);
--- serialization/string/samples/pubsub/hello_send/src/hello_send.cpp
+++ serialization/string/samples/pubsub/hello_send/src/hello_send.cpp
@@ -17,7 +17,12 @@
  * ========================= eCAL LICENSE =================================
 */
 
+// Include the basic eCAL header
 #include <ecal/ecal.h>
+/* 
+  We want to publish raw strings, so wie include the string publisher header.
+  eCAL supports multiple message formats.
+*/
 #include <ecal/msg/string/publisher.h>
 
 #include <iostream>
@@ -26,32 +31,51 @@
 int main()
 {
   std::cout << "-------------------------------" << std::endl;
-  std::cout << " HELLO WORLD SENDER"             << std::endl;
+  std::cout << " C++: HELLO WORLD SENDER"        << std::endl;
   std::cout << "-------------------------------" << std::endl;
 
-  // initialize eCAL API
+  /* 
+    Initialize eCAL. You always have to initialize eCAL beforee using its API.
+    The name of our eCAL Process will be "hello_send". 
+    This name will be visible in the eCAL Monitor, once the process is running.
+  */
   eCAL::Initialize("hello_send");
 
-  // publisher for topic "Hello"
+  /*
+    Creating the eCAL Publisher. An eCAL Process can create multiple publishers (and subscribers).
+    The topic we are going to publish is called "hello".
+  */
   eCAL::string::CPublisher pub("hello");
 
-  // send updates
+  /*
+    Creating an inifite publish-loop.
+    eCAL Supports a stop signal; when an eCAL Process is stopped, eCAL::Ok() will return false.
+  */
   int cnt = 0;
   while (eCAL::Ok())
   {
-    // build string
+    /*
+      Build the string you want to send, using a stringstream in this example.
+    */
     std::stringstream snd_content;
     snd_content << "HELLO WORLD FROM C++" << " (" << ++cnt << ")";
 
-    // send content
+    /*
+      Send the content to other eCAL Processes that have subscribed to the topic "hello".
+    */
     pub.Send(snd_content.str(), cnt);
     std::cout << "Sent \"" << snd_content.str() << "\"" << std::endl;
 
-    // sleep 10 ms
+    /*
+      Sleep for 500 ms so we send with a frequency of 2 Hz.
+    */
     eCAL::Process::SleepMS(500);
   }
 
-  // finalize eCAL API
+  /*
+    Deinitialize eCAL.
+    You should always do that before your application exits.
+  */
   eCAL::Finalize();
 
   return(0);
