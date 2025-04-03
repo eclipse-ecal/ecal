90b14380fc97463e05d12bceb25ed7aa48e4f4c7
KerstinKeller <KerstinKeller@users.noreply.github.com>
2025-04-03 11:10:24 +0200

    [bugfix] Allow compatibility with CMake 4.0 (#2103)
    Co-authored-by: Kerstin Keller <13848742+KerstinKeller@users.noreply.github.com>
    Co-authored-by: DownerCase <119755054+DownerCase@users.noreply.github.com>

--- thirdparty/termcolor/build-termcolor.cmake
+++ thirdparty/termcolor/build-termcolor.cmake
@@ -1,3 +1,6 @@
 include_guard(GLOBAL)
 
+ecal_variable_push(CMAKE_POLICY_VERSION_MINIMUM)
+set(CMAKE_POLICY_VERSION_MINIMUM 3.5)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/termcolor thirdparty/termcolor EXCLUDE_FROM_ALL SYSTEM)
+ecal_variable_pop(CMAKE_POLICY_VERSION_MINIMUM)
