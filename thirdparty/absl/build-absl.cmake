04d81c04bd1f90c3b369ab753bf514051a92ee2a
DownerCase <downercase8@gmail.com>
2025-04-10 17:49:53 +0100

    Fixup build-absl.cmake

--- thirdparty/absl/build-absl.cmake
+++ thirdparty/absl/build-absl.cmake
@@ -1,4 +1,9 @@
 include_guard(GLOBAL)
 
-set(ABSL_ENABLE_INSTALL "ON")
-add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/absl" thirdparty/absl SYSTEM)
+# Copied out of protobuf/cmake/abseil-cpp.cmake
+if(protobuf_INSTALL)
+  # When protobuf_INSTALL is enabled and Abseil will be built as a module,
+  # Abseil will be installed along with protobuf for convenience.
+  set(ABSL_ENABLE_INSTALL ON)
+endif()
+add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/absl" "${eCAL_BINARY_DIR}thirdparty/absl" SYSTEM)
