f4dfd1ba7b253409a7b9d0f330836c949861cd2d
Kerstin Keller <13848742+KerstinKeller@users.noreply.github.com>
2025-04-29 15:18:06 +0200

    Client implementation now working Added tests for client / server communication.

--- lang/python/nanobind_core/src/core/service/py_client_instance.cpp
+++ lang/python/nanobind_core/src/core/service/py_client_instance.cpp
@@ -1,118 +1,130 @@
-///* ========================= eCAL LICENSE =================================
-// *
-// * Copyright (C) 2016 - 2025 Continental Corporation
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// * 
-// *      http://www.apache.org/licenses/LICENSE-2.0
-// * 
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// *
-// * ========================= eCAL LICENSE =================================
-//*/
-//
-///**
-// * @file   service/client_instance.h
-// * @brief  eCAL client instance interface
-//**/
-//
-//#pragma once
-//
-//#include <ecal/deprecate.h>
-//#include <ecal/namespace.h>
-//#include <ecal/os.h>
-//
-//#include <ecal/service/types.h>
-//
-//#include <memory>
-//#include <string>
-//
-//namespace eCAL
-//{
-//  class CServiceClientImpl;
-//
-//  class ECAL_API_CLASS CClientInstance final
-//  {
-//  public:
-//    ECAL_API_EXPORTED_MEMBER
-//      static constexpr long long DEFAULT_TIME_ARGUMENT = -1;  /*!< Use DEFAULT_TIME_ARGUMENT in the `CallWithResponse()` and `CallWithCallback()` functions for blocking calls */
-//
-//    // Constructor
-//    ECAL_API_EXPORTED_MEMBER
-//      CClientInstance(const SEntityId& entity_id_, const std::shared_ptr<CServiceClientImpl>& service_client_id_impl_);
-//
-//    // Defaulted destructor
-//    ~CClientInstance() = default;
-//
-//    // Deleted copy constructor and copy assignment operator
-//    CClientInstance(const CClientInstance&) = delete;
-//    CClientInstance& operator=(const CClientInstance&) = delete;
-//
-//    // Defaulted move constructor and move assignment operator
-//    ECAL_API_EXPORTED_MEMBER CClientInstance(CClientInstance&& rhs) noexcept = default;
-//    ECAL_API_EXPORTED_MEMBER CClientInstance& operator=(CClientInstance&& rhs) noexcept = default;
-//
-//    /**
-//     * @brief Blocking call of a service method, response will be returned as pair<bool, SServiceReponse>
-//     *
-//     * @param       method_name_  Method name.
-//     * @param       request_      Request string.
-//     * @param       timeout_ms_   Maximum time before operation returns (in milliseconds, DEFAULT_TIME_ARGUMENT means infinite).
-//     *
-//     * @return  success state and service response
-//    **/
-//    ECAL_API_EXPORTED_MEMBER
-//      std::pair<bool, SServiceResponse> CallWithResponse(const std::string& method_name_, const std::string& request_, int timeout_ms_ = DEFAULT_TIME_ARGUMENT);
-//
-//    /**
-//     * @brief Blocking call of a service method, using callback
-//     *
-//     * @param method_name_        Method name.
-//     * @param request_            Request string.
-//     * @param response_callback_  Callback function for the service method response.
-//     * @param timeout_ms_         Maximum time before operation returns (in milliseconds, DEFAULT_TIME_ARGUMENT means infinite).
-//     *
-//     * @return  True if successful.
-//    **/
-//    ECAL_API_EXPORTED_MEMBER
-//      bool CallWithCallback(const std::string& method_name_, const std::string& request_, const ResponseCallbackT& response_callback_, int timeout_ms_ = DEFAULT_TIME_ARGUMENT);
-//
-//    /**
-//     * @brief Asynchronous call of a service method, using callback
-//     *
-//     * @param method_name_        Method name.
-//     * @param request_            Request string.
-//     * @param response_callback_  Callback function for the service method response.
-//     *
-//     * @return  True if successful.
-//    **/
-//    ECAL_API_EXPORTED_MEMBER
-//      bool CallWithCallbackAsync(const std::string& method_name_, const std::string& request_, const ResponseCallbackT& response_callback_);
-//
-//    /**
-//     * @brief Check connection state.
-//     *
-//     * @return  True if connected, false if not.
-//    **/
-//    ECAL_API_EXPORTED_MEMBER
-//      bool IsConnected() const;
-//
-//    /**
-//     * @brief Get unique client entity id.
-//     *
-//     * @return  The client entity id.
-//    **/
-//    ECAL_API_EXPORTED_MEMBER
-//      SEntityId GetClientID() const;
-//
-//  private:
-//    SEntityId                                       m_entity_id;
-//    const std::shared_ptr<eCAL::CServiceClientImpl> m_service_client_impl;
-//  };
-//}
+/* ========================= eCAL LICENSE =================================
+ *
+ * Copyright (C) 2016 - 2025 Continental Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ========================= eCAL LICENSE =================================
+*/
+
+#include <core/service/py_client_instance.h>
+#include <ecal/service/client_instance.h>
+
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
+#include <nanobind/stl/set.h>
+#include <nanobind/stl/tuple.h>
+#include <nanobind/stl/pair.h>
+
+#include <helper/bytestring_property.h>
+#include <helper/make_gil_safe_shared.h>
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+void AddServiceClientInstance(nanobind::module_& m)
+{
+  // Bind ClientInstance but disable Python construction
+  nb::class_<eCAL::CClientInstance>(m, "ClientInstance", 
+    "Handle to a specific eCAL service client instance.  \n"
+    "Cannot be constructed directly from Python; you get it from the ServiceClient API."
+    )
+    // No .def(nb::init<…>()) here! -  Class shall not be constructible from Python
+
+    .def("call_with_response",
+      [](eCAL::CClientInstance& self,
+        const std::string& method_name,
+        nb::bytes python_request,
+        int timeout_ms) -> std::pair<bool, eCAL::SServiceResponse>
+      {
+        // Convert Python bytes to std::string
+        std::string cpp_request{ nb_bytes_to_std_string(python_request) };
+        std::pair<bool, eCAL::SServiceResponse> result;
+        {
+          // Release the GIL while we do the blocking C++ call
+          nb::gil_scoped_release release;
+          result = self.CallWithResponse(method_name, cpp_request, timeout_ms);
+        }
+        // GIL is re-acquired here
+        return result;
+      },
+      nb::arg("method_name"),
+      nb::arg("request"),
+      nb::arg("timeout_ms") = eCAL::CClientInstance::DEFAULT_TIME_ARGUMENT,
+      "Blocking call on a single server instance; request is bytes, releases GIL, "
+      "returns (success: bool, ServiceResponse).")
+
+    .def("call_with_callback",
+      [](eCAL::CClientInstance& self,
+        const std::string& method_name,
+        const nb::bytes& python_request,
+        const nb::callable& python_callback,
+        int timeout_ms) {
+          auto python_callback_pointer = make_gil_safe_shared<nb::callable>(python_callback);
+          eCAL::ResponseCallbackT cpp_callback = [python_callback_pointer](const eCAL::SServiceResponse& resp) {
+            nb::gil_scoped_acquire acquire;
+            try { (*python_callback_pointer)(resp); }
+            catch (const std::exception& e) {
+              std::cerr << "Response callback error: " << e.what() << std::endl;
+            }
+          };
+          // Do the conversion before releasing the GIL
+          std::string cpp_request{ nb_bytes_to_std_string(python_request) };
+
+          nb::gil_scoped_release release;
+          return self.CallWithCallback(method_name, cpp_request, cpp_callback, timeout_ms);
+      },
+      nb::arg("method_name"),
+      nb::arg("request"),
+      nb::arg("callback"),
+      nb::arg("timeout_ms") = eCAL::CClientInstance::DEFAULT_TIME_ARGUMENT,
+      "Blocking call with callback.")
+
+    .def("call_with_callback_async",
+      [](eCAL::CClientInstance& self,
+        const std::string& method_name,
+        const nb::bytes& python_request,
+        const nb::callable& python_callback) {
+          auto python_callback_pointer = make_gil_safe_shared<nb::callable>(python_callback);
+          eCAL::ResponseCallbackT cpp_callback = [python_callback_pointer](const eCAL::SServiceResponse& resp) {
+            nb::gil_scoped_acquire acquire;
+            try { (*python_callback_pointer)(resp); }
+            catch (const std::exception& e) {
+              std::cerr << "Async callback error: " << e.what() << std::endl;
+            }
+          };
+          // Do the conversion before releasing the GIL
+          std::string cpp_request{ nb_bytes_to_std_string(python_request) };
+
+          nb::gil_scoped_release release;
+          return self.CallWithCallbackAsync(method_name, cpp_request, cpp_callback);
+      },
+      nb::arg("method_name"),
+      nb::arg("request"),
+      nb::arg("callback"),
+      "Asynchronous call with callback.")
+
+    .def("is_connected",
+      &eCAL::CClientInstance::IsConnected,
+      "Check whether this client instance is connected.")
+
+    .def("get_client_id",
+      &eCAL::CClientInstance::GetClientID,
+      "Return the underlying SEntityId of this client instance.")
+
+    .def("__repr__",
+      [](const eCAL::CClientInstance& self) {
+        auto id = self.GetClientID();
+        return "<ClientInstance entity_id=" + std::to_string(id.entity_id) + ">";
+      });
+}
--- lang/python/tests/nanobind_core/types_test.py
+++ lang/python/tests/nanobind_core/types_test.py
@@ -1,3 +1,21 @@
+# ========================= eCAL LICENSE =================================
+#
+# Copyright (C) 2016 - 2025 Continental Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ========================= eCAL LICENSE =================================
+ 
 import ecal.nanobind_core as ecal_core
 
 EMPTY_STRING = ""
@@ -6,33 +24,34 @@ MY_NAME = "name"
 MY_ENCODING = "encoding"
 MY_DESCRIPTOR = b"descriptor"
 
-def test_constructor_empty():
+class TestDataTypeInformation:
+  def test_constructor_empty(self):
     default_datatype_info = ecal_core.DataTypeInformation()
     assert default_datatype_info.name == EMPTY_STRING
     assert default_datatype_info.encoding == EMPTY_STRING
     assert default_datatype_info.descriptor == EMPTY_BYTES
-
-def test_constructor_name():
+  
+  def test_constructor_name(self):
     default_datatype_info = ecal_core.DataTypeInformation(name=MY_NAME)
     assert default_datatype_info.name == MY_NAME
     assert default_datatype_info.encoding == EMPTY_STRING
     assert default_datatype_info.descriptor == EMPTY_BYTES
-
-def test_constructor_encoding():
+  
+  def test_constructor_encoding(self):
     default_datatype_info = ecal_core.DataTypeInformation(encoding=MY_ENCODING)
     assert default_datatype_info.name == EMPTY_STRING
     assert default_datatype_info.encoding == MY_ENCODING
     assert default_datatype_info.descriptor == EMPTY_BYTES
-
-def test_constructor_type():
+  
+  def test_constructor_type(self):
     default_datatype_info = ecal_core.DataTypeInformation(descriptor=MY_DESCRIPTOR)
     assert default_datatype_info.name == EMPTY_STRING
     assert default_datatype_info.encoding == EMPTY_STRING
     assert default_datatype_info.descriptor == MY_DESCRIPTOR
-
-def test_constructor_full():
+  
+  def test_constructor_full(self):
     default_datatype_info = ecal_core.DataTypeInformation(
-        name=MY_NAME, encoding=MY_ENCODING, descriptor=MY_DESCRIPTOR
+      name=MY_NAME, encoding=MY_ENCODING, descriptor=MY_DESCRIPTOR
     )
     assert default_datatype_info.name == MY_NAME
     assert default_datatype_info.encoding == MY_ENCODING
