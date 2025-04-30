c547a26562634b83536da1eea0ab3c879a864665
Kerstin Keller <13848742+KerstinKeller@users.noreply.github.com>
2025-04-30 14:38:26 +0200

    Adapt the Python samples.

--- lang/python/samples/nanobind_core/service/nb_binary_mirror_client.py
+++ lang/python/samples/nanobind_core/service/nb_binary_mirror_client.py
@@ -18,48 +18,84 @@
 
 from pickle import TRUE
 import sys
+import textwrap
 import time
 
 import ecal.nanobind_core as ecal_core
 
-  # define the client response callback to catch server responses
-def client_resp_callback(service_info, response):
-    if (service_info["call_state"] == "call_state_executed"):
-        print("'DemoService' method '{}' responded : '{}'".format(service_info["method_name"], response))
-        print()
-    else:
-        print("server {} response failed, error : '{}'".format(service_info["host_name"], service_info["error_msg"]))
-        print()
+def print_service_response(service_response : ecal_core.ServiceResponse):
+  print(f"Received service response in Python: {service_response.call_state}")
+  print(f"Method    : {service_response.service_method_information.method_name}")
+  print(f"Response  : {service_response.response}")
+  print(f"Server ID : {service_response.server_id.service_id.entity_id}")
+  print(f"Host      : {service_response.server_id.service_id.host_name}")
+  print()
 
 # generator, alternating returns "echo", "reverse"
-def get_method():
+def generate_method_name():
   methods = ["echo", "reverse"]
   while True:
     for method in methods:
       yield method 
 
 def main():
-  # print eCAL version and date
-  print("eCAL {} ({})\n".format(ecal_core.get_version_string(), ecal_core.get_version_date_string()))
-  
-  # initialize eCAL API
+  print(textwrap.dedent("""
+    -----------------------
+     Python: MIRROR CLIENT
+    -----------------------
+  """).strip())
+          
+  # First, initialize the eCAL API.
   ecal_core.initialize("mirror client python")
+
+  # Print eCAL version and date.
+  print(f"eCAL {ecal_core.get_version_string()} ({ecal_core.get_version_date_string()})")
+  ecal_core.process.set_state(ecal_core.process.Severity.WARNING, ecal_core.process.SeverityLevel.LEVEL1, "Waiting for a server");
   
-  # create a client for the "mirror" service
-  client = ecal_core.ServiceClient("mirror")
-    
-  # and add it to the client
-  client.add_response_callback(client_resp_callback)
+  # Create a client for the "mirror" service
+  mirror_client = ecal_core.ServiceClient("mirror")
+  
+  # Wait until the client is connected to a server,
+  # so we don't call methods that are not available.
+  while (not mirror_client.is_connected()):
+    print("Waiting for a service ...")
+    time.sleep(1.0)
+
+  # Now that we are connected, we can set the process state to "healthy" and communicate the connection.
+  ecal_core.process.set_state(ecal_core.process.Severity.HEALTHY, ecal_core.process.SeverityLevel.LEVEL1, "Connected!");
+  
+  # instantiate the generator to switch between calls to "echo" and "reverse".
+  method_generator = generate_method_name()
 
   # idle and call service methods
   while(ecal_core.ok()):
-    request = bytes("stressed", "ascii")
-    client.call(get_method(), request)
-    time.sleep(0.5)
 
-  # destroy client
-  client.destroy()
-  
+    # We iterate now over all client instances and call the methods by name.
+    # With this approach we have the option to filter out client instances that we don't want to call.
+
+    service_request = bytes("stressed", "ascii")
+    service_method_name = next(method_generator)
+
+    client_instances = mirror_client.get_client_instances()
+    for client_instance in client_instances:
+        
+        # There are three ways that a client can call the server
+        # 1. Blocking, with a response 
+        (ok, response) = client_instance.call_with_response(service_method_name, service_request, timeout_ms=1000)
+        if (response):
+          print_service_response(response)
+        time.sleep(0.5)
+
+        # 2. Blocking with callback
+        ok = client_instance.call_with_callback(service_method_name, service_request, print_service_response, timeout_ms=1000)
+        time.sleep(0.5)
+
+        # 3. Asynchronous with callback
+        ok = client_instance.call_with_callback_async(service_method_name, service_request, print_service_response)
+        time.sleep(0.5)
+
+  del mirror_client
+
   # finalize eCAL API
   ecal_core.finalize()
 
