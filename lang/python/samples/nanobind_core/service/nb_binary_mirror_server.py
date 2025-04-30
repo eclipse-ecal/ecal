f89e7e16fe07153db76797618474bce3987e812f
Kerstin Keller <13848742+KerstinKeller@users.noreply.github.com>
2025-04-30 14:38:26 +0200

    Adapt the Python samples.

--- lang/python/samples/nanobind_core/service/nb_binary_mirror_server.py
+++ lang/python/samples/nanobind_core/service/nb_binary_mirror_server.py
@@ -17,17 +17,18 @@
 # ========================= eCAL LICENSE =================================
 
 import time
+import textwrap
 from typing import Tuple
 
 import ecal.nanobind_core as ecal_core
 
 def print_data(method_name : str, request : bytes, response : bytes):
-  print("Method   : '{}' called".format(method_name))
-  print("Requests : {}".format(request))
-  print("Response : {}".format(response))
+  print(f"Method   : '{method_name}' called in Python")
+  print(f"Requests : {request}")
+  print(f"Response : {response}")
   print()
 
-# define the server method "ping" function
+# Define the callback function that will be called when a client calls the service method "echo".
 def echo_req_callback(
     method_information : ecal_core.ServiceMethodInformation,
     request : bytes) -> Tuple[int, bytes]:
@@ -35,7 +36,7 @@ def echo_req_callback(
   print_data(method_information.method_name, request, response)
   return 0, response
 
-# define the server method "ping" function
+# Define the callback function that will be called when a client calls the service method "reverse".
 def reverse_req_callback(
     method_information : ecal_core.ServiceMethodInformation,
     request : bytes) -> Tuple[int, bytes]:
@@ -44,33 +45,37 @@ def reverse_req_callback(
   return 0, response
 
 def main():
-  # print eCAL version and date
-  print("eCAL {} ({})\n".format(ecal_core.get_version_string(), ecal_core.get_version_date_string()))
-  
-  # initialize eCAL API
+  print(textwrap.dedent("""
+    -----------------------
+     Python: MIRROR SERVER
+    -----------------------
+  """).strip())
+          
+  # First, initialize the eCAL API.
   ecal_core.initialize("mirror server python")
-  
-  # create a server for the "DemoService" service
+
+  # Print eCAL version and date.
+  print(f"eCAL {ecal_core.get_version_string()} ({ecal_core.get_version_date_string()})")
+  ecal_core.process.set_state(ecal_core.process.Severity.HEALTHY, ecal_core.process.SeverityLevel.LEVEL1, "I feel good");
+
+  # Create the mirror server and give it the name "mirror".
   server = ecal_core.ServiceServer("mirror")
 
-  # define service methods and add them to the callbacks
-  echo_method_info = ecal_core.ServiceMethodInformation()
-  echo_method_info.method_name = "echo"
+  # The server will have two methods: "echo" and "reverse".
+  # To set a callback, we need to set a ServiceMethodInformation struct as well as the callback function.
+  echo_method_info = ecal_core.ServiceMethodInformation(method_name="echo")
   server.set_method_callback(echo_method_info, echo_req_callback)
-
-  reverse_method_info = ecal_core.ServiceMethodInformation()
-  reverse_method_info.method_name = "reverse"
+  reverse_method_info = ecal_core.ServiceMethodInformation(method_name="reverse")
   server.set_method_callback(reverse_method_info, reverse_req_callback)
   
-  # we can retrieve the services name / id
+  # We can retrieve the services name / id.
   print(server.get_service_name())
   print(server.get_service_id())
 
-  # we can check if clients have connected to this service
+  # We can check if clients have connected to this service.
   print(server.is_connected())
 
-
-  # idle
+  # Now we will go in an infinite loop, to wait for incoming service calls that will be handled with the callbacks.
   while(ecal_core.ok()):
     time.sleep(0.1)
 
