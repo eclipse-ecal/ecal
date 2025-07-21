d126447934c077006ecb8b9194edeb0297fe8fd2
Kristof Hannemann <50989282+hannemn@users.noreply.github.com>
2025-07-21 12:21:03 +0200

    Provoked gOpenNamedEvent failure with test case

--- ecal/tests/cpp/event_test/src/event_test.cpp
+++ ecal/tests/cpp/event_test/src/event_test.cpp
@@ -21,6 +21,39 @@
 
 #include <gtest/gtest.h>
 #include <string>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+class Barrier 
+{
+public:
+  Barrier(int count) : thread_count(count), waiting(0), step(0) 
+  {}
+
+  void wait() 
+  {
+    std::unique_lock<std::mutex> lock(mtx);
+    int current_step = step;
+
+    if (++waiting == thread_count) {
+      waiting = 0;       // Reset for the next use
+      ++step;            // Increment the step to release all threads
+      cv.notify_all();   // Release all waiting threads
+    }
+    else {
+      cv.wait(lock, [this, current_step] { return current_step != step; });
+    }
+  }
+
+private:
+  int thread_count;
+  std::atomic<int> waiting;
+  int step;
+  std::mutex mtx;
+  std::condition_variable cv;
+};
+
 
 TEST(core_cpp_core, Event_EventSetGet)
 { 
@@ -39,4 +72,31 @@ TEST(core_cpp_core, Event_EventSetGet)
 
   // get set event
   EXPECT_EQ(true, gWaitForEvent(event_handle, 100));
+}
+
+TEST(core_cpp_core, Event_OpenEventInParallel)
+{
+  // parameter
+  const std::string event_name = "my_parallel_event";
+  const int runs = 100;
+
+  Barrier barrier(2);
+
+      
+  auto event_worker = [&barrier, &event_name, runs](bool has_ownership)
+  {
+      for (int i = 0; i < runs; ++i)
+      {
+        eCAL::EventHandleT event_handle;
+        barrier.wait();
+        EXPECT_EQ(true, eCAL::gOpenNamedEvent(&event_handle, event_name, true));
+        barrier.wait();
+        eCAL::gCloseEvent(event_handle);
+      }
+  };
+
+  std::thread event_worker_thread_1(event_worker, true);
+  std::thread event_worker_thread_2(event_worker, false);
+  event_worker_thread_1.join();
+  event_worker_thread_2.join();
 }
