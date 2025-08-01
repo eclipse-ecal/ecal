f091f1e94eeb3d2fa6305208bd19d7d3ab209b75
Kristof Hannemann <50989282+hannemn@users.noreply.github.com>
2025-08-01 14:29:02 +0200

    [lang] Added deprecation warning for legacy python API (#2296)

---
+++ lang/python/src/core/ecal/proto/__init__.py
@@ -0,0 +1,2 @@
+import warnings
+warnings.warn("The module ecal.proto is deprecated and will be removed in a future eCAL version. Please use the module ecal.msg.proto instead.", DeprecationWarning,stacklevel=2)
