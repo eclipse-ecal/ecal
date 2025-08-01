c5cfeb04644bd36b13d591491c6a7fbf27ca6873
Kristof Hannemann <50989282+hannemn@users.noreply.github.com>
2025-08-01 14:29:02 +0200

    [lang] Added deprecation warning for legacy python API (#2296)

---
+++ lang/python/src/core/ecal/core/__init__.py
@@ -0,0 +1,2 @@
+import warnings
+warnings.warn("The module ecal.core is deprecated and will be removed in a future eCAL version. Please use the module ecal.nanobind_core instead.", DeprecationWarning,stacklevel=2)
