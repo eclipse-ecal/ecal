4344d489d4444e9a6710e8296a4ff9ca18b91e25
Kristof Hannemann <50989282+hannemn@users.noreply.github.com>
2025-10-09 14:02:57 +0200

    Test case for touch() method

--- ecal/tests/cpp/expmap_test/src/expmap_test.cpp
+++ ecal/tests/cpp/expmap_test/src/expmap_test.cpp
@@ -301,3 +301,21 @@ TEST(core_cpp_core, ExpMap_Remove)
   EXPECT_EQ(0, expmap.size());
   EXPECT_FALSE(expmap.erase("B"));
 }
+
+TEST(core_cpp_core, ExpMap_Touch)
+{
+  eCAL::Util::CExpirationMap<std::string, int, TestingClock> expmap(std::chrono::milliseconds(200));
+  expmap["A"] = 1;
+  expmap["B"] = 2;
+  EXPECT_EQ(2, expmap.size());
+  TestingClock::increment_time(std::chrono::milliseconds(150));
+  expmap.touch("A");
+  // B gets timeouted here
+  TestingClock::increment_time(std::chrono::milliseconds(150)); 
+  exmap.erased_expired();
+  // only A remains in the map
+  EXPECT_EQ(1, expmap.size());
+  EXPECT_EG(1, expmap["A"]);
+  TestingClock::increment_time(std::chrono::milliseconds(150));
+  EXPECT_EQ(0, expmap.size());
+}
