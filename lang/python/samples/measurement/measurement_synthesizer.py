d1c23e3b3d5ff8139ad6e3ee15f1586b9717890b
Kristof Hannemann <50989282+hannemn@users.noreply.github.com>
2025-11-03 11:08:29 +0100

    Update lang/python/samples/measurement/measurement_synthesizer.py
    Co-authored-by: KerstinKeller <KerstinKeller@users.noreply.github.com>

--- lang/python/samples/measurement/measurement_synthesizer.py
+++ lang/python/samples/measurement/measurement_synthesizer.py
@@ -1,6 +1,6 @@
 # ========================= eCAL LICENSE =================================
 #
-# Copyright (C) 2025 Aumovio
+# Copyright 2025 AUMOVIO and subsidiaries. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
