236338a6aefad43be4b27ee706358e78f0a35a7b
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2025-11-20 12:14:39 +0100

    re-applied changes that were undone by accident

--- ecal/core/src/service/ecal_service_client_impl.cpp
+++ ecal/core/src/service/ecal_service_client_impl.cpp
@@ -126,11 +126,12 @@ namespace eCAL
       const std::lock_guard<std::mutex> lock(m_event_callback_mutex);
       m_event_callback = event_callback_;
     }
-
+    
     // Send registration sample
-    if (!m_service_name.empty() && g_registration_provider() != nullptr)
+    auto registration_provider = g_registration_provider();
+    if (!m_service_name.empty() && registration_provider)
     {
-      g_registration_provider()->RegisterSample(GetRegistrationSample());
+      registration_provider->RegisterSample(GetRegistrationSample());
 #ifndef NDEBUG
       eCAL::Logging::Log(eCAL::Logging::log_level_debug2, "CServiceClientImpl::CServiceClientImpl: Registered client with service name: " + m_service_name);
 #endif
@@ -157,9 +158,10 @@ namespace eCAL
     }
 
     // Send unregistration sample
-    if (g_registration_provider() != nullptr)
+    auto registration_provider = g_registration_provider();
+    if (registration_provider)
     {
-      g_registration_provider()->UnregisterSample(GetUnregistrationSample());
+      registration_provider->UnregisterSample(GetUnregistrationSample());
 #ifndef NDEBUG
       eCAL::Logging::Log(eCAL::Logging::log_level_debug2, "CServiceClientImpl::~CServiceClientImpl: Unregistered client for service name: " + m_service_name);
 #endif
@@ -207,22 +209,10 @@ namespace eCAL
       response_callback_(response.second);
     }
 
-    // Handle timeout event
-    if (!response.first && response.second.call_state == eCallState::timeouted)
-    {
-      SServiceId service_id;
-      service_id.service_name = m_service_name;
-      service_id.service_id = entity_id_;
-      NotifyEventCallback(service_id, eClientEvent::timeout);
-#ifndef NDEBUG
-      eCAL::Logging::Log(eCAL::Logging::log_level_debug1, "CServiceClientImpl::CallWithCallback: Synchronous call for service: " + m_service_name + ", method: " + method_name_ + " timed out.");
-#endif
-    }
-
     return response;
   }
 
-  // Asynchronous call to a service with a specified timeout
+  // Asynchronous call to a service
   bool CServiceClientImpl::CallWithCallbackAsync(const SEntityId & entity_id_, const std::string & method_name_, const std::string & request_, const ResponseCallbackT & response_callback_)
   {
 #ifndef NDEBUG
