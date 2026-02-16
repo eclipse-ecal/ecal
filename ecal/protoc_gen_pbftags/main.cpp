0194286dd13ca3fe2eea5e0e0b071c5af1a03748
Peguen <73380451+Peguen@users.noreply.github.com>
2026-02-16 10:52:51 +0100

    [protozero] Fix missing header for Ubuntu 20 compilation. (#2522)

--- ecal/protoc_gen_pbftags/main.cpp
+++ ecal/protoc_gen_pbftags/main.cpp
@@ -20,6 +20,7 @@
 #include <google/protobuf/compiler/code_generator.h>
 #include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
 #include <google/protobuf/stubs/common.h>
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/descriptor.pb.h>
