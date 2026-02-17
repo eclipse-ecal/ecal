83412784a10ce3f2fcfef84a69b9c1262166fd7d
Kerstin Keller <13848742+KerstinKeller@users.noreply.github.com>
2026-02-17 09:03:40 +0100

    More review comments.

--- lib/ecal_utils/tests/dynamic_library.cpp
+++ lib/ecal_utils/tests/dynamic_library.cpp
@@ -2,7 +2,7 @@
  *
  * Copyright 2026 AUMOVIO and subsidiaries. All rights reserved.
  *
- * Licensed under the fpache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
