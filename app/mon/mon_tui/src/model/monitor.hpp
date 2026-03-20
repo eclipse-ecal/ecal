7df52dbd62947883f5db5e6a6f327a77a7713f8b
KerstinKeller <KerstinKeller@users.noreply.github.com>
2026-03-20 11:00:21 +0100

    mon_tui: migrate from protobuf to struct based monitoring / logging API (#2565)

--- app/mon/mon_tui/src/model/monitor.hpp
+++ app/mon/mon_tui/src/model/monitor.hpp
@@ -22,26 +22,21 @@
 
 #include <ecal/ecal.h>
 #include <ecal/monitoring.h>
+#include <ecal/process_severity.h>
 
 #include <functional>
+#include <map>
+#include <mutex>
 #include <string>
+#include <thread>
+#include <unordered_map>
 #include <utility>
 #include <vector>
-#include <thread>
-#include <map>
 
 #include "model/data/host.hpp"
 #include "model/data/process.hpp"
 #include "model/data/service.hpp"
 #include "model/data/topic.hpp"
-
-#ifdef _MSC_VER
-#pragma warning(push, 0) // disable proto warnings
-#endif
-#include "ecal/core/pb/monitoring.pb.h"
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
 
 using ModelUpdateCallbackT = std::function<void()>;
 
@@ -49,7 +44,7 @@ class MonitorModel
 {
   int update_time_ = 1000;
   bool is_polling;
-  eCAL::pb::Monitoring mon_;
+  eCAL::Monitoring::SMonitoring mon_;
 
   std::vector<Host> hosts;
   std::vector<Process> processes;
@@ -61,78 +56,68 @@ class MonitorModel
   mutable std::mutex mtx;
   mutable std::mutex callback_mtx;
 
-  Process::Severity Severity(eCAL::pb::eProcessSeverity severity)
+  Process::Severity Severity(int32_t severity)
   {
-    switch(severity)
+    switch(static_cast<eCAL::Process::eSeverity>(severity))
     {
-    case eCAL::pb::proc_sev_healthy:
+    case eCAL::Process::eSeverity::healthy:
       return Process::Severity::HEALTHY;
-    case eCAL::pb::proc_sev_warning:
+    case eCAL::Process::eSeverity::warning:
       return Process::Severity::WARNING;
-    case eCAL::pb::proc_sev_critical:
+    case eCAL::Process::eSeverity::critical:
       return Process::Severity::CRITICAL;
-    case eCAL::pb::proc_sev_failed:
+    case eCAL::Process::eSeverity::failed:
       return Process::Severity::FAILED;
-    case eCAL::pb::proc_sev_unknown:
+    case eCAL::Process::eSeverity::unknown:
     default:
       return Process::Severity::UNKNOWN;
     }
   }
 
-  Process::SeverityLevel SeverityLevel(eCAL::pb::eProcessSeverityLevel level)
+  Process::SeverityLevel SeverityLevel(int32_t level)
   {
-    switch(level)
+    switch(static_cast<eCAL::Process::eSeverityLevel>(level))
     {
-    case eCAL::pb::proc_sev_level1:
+    case eCAL::Process::eSeverityLevel::level1:
       return Process::SeverityLevel::LEVEL_1;
-    case eCAL::pb::proc_sev_level2:
+    case eCAL::Process::eSeverityLevel::level2:
       return Process::SeverityLevel::LEVEL_2;
-    case eCAL::pb::proc_sev_level3:
+    case eCAL::Process::eSeverityLevel::level3:
       return Process::SeverityLevel::LEVEL_3;
-    case eCAL::pb::proc_sev_level4:
+    case eCAL::Process::eSeverityLevel::level4:
       return Process::SeverityLevel::LEVEL_4;
-    case eCAL::pb::proc_sev_level5:
+    case eCAL::Process::eSeverityLevel::level5:
       return Process::SeverityLevel::LEVEL_5;
-    case eCAL::pb::proc_sev_level_unknown:
     default:
       return Process::SeverityLevel::UNKNOWN;
     }
   }
 
-  Process::TimeSyncState TimeSyncState(eCAL::pb::eTimeSyncState state)
+  Process::TimeSyncState TimeSyncState(int32_t state)
   {
     switch(state)
     {
-    case eCAL::pb::tsync_realtime:
+    case 1:
       return Process::TimeSyncState::REALTIME;
-    case eCAL::pb::tsync_replay:
-      return Process::TimeSyncState::REALTIME;
-    case eCAL::pb::tsync_none:
+    case 2:
+      return Process::TimeSyncState::REPLAY;
+    case 0:
     default:
       return Process::TimeSyncState::NONE;
     }
   }
 
-  Topic::Direction TopicDirection(const std::string &direction)
-  {
-    if(direction == "publisher") return Topic::Direction::PUBLISHER;
-
-    return Topic::Direction::SUBSCRIBER;
-  }
-
-  Topic::TransportLayer TopicTransportLayer(eCAL::pb::eTransportLayerType layer)
+  Topic::TransportLayer TopicTransportLayer(eCAL::Monitoring::eTransportLayerType layer)
   {
     switch(layer)
     {
-      case eCAL::pb::tl_all:
-        return Topic::TransportLayer::ALL;
-      case eCAL::pb::tl_ecal_shm:
+      case eCAL::Monitoring::eTransportLayerType::shm:
         return Topic::TransportLayer::SHM;
-      case eCAL::pb::tl_ecal_tcp:
+      case eCAL::Monitoring::eTransportLayerType::tcp:
         return Topic::TransportLayer::TCP;
-      case eCAL::pb::tl_ecal_udp_mc:
+      case eCAL::Monitoring::eTransportLayerType::udp_mc:
         return Topic::TransportLayer::UDP_MC;
-      case eCAL::pb::tl_none:
+      case eCAL::Monitoring::eTransportLayerType::none:
       default:
         return Topic::TransportLayer::NONE;
     }
@@ -148,93 +133,98 @@ class MonitorModel
     topics.clear();
 
     std::unordered_map<std::string, Host*> hosts_map;
-    for(auto &p: *mon_.mutable_processes())
+    for(const auto &p : mon_.processes)
     {
-      if(hosts_map.find(p.host_name()) == hosts_map.end())
+      if(hosts_map.find(p.host_name) == hosts_map.end())
       {
         auto &host = hosts.emplace_back();
-        host.name = p.host_name();
+        host.name = p.host_name;
         hosts_map[host.name] = &host;
       }
       auto &process = processes.emplace_back();
-      process.process_id = p.process_id();
-      process.name = std::move(*p.mutable_process_name());
-      process.host_name = std::move(*p.mutable_host_name());
-      process.unit_name = std::move(*p.mutable_unit_name());
-      process.params = std::move(*p.mutable_process_parameter());
-      process.severity = Severity(p.state().severity());
-      process.severity_level = SeverityLevel(p.state().severity_level());
-      process.state_info = std::move(*p.mutable_state()->mutable_info());
-      process.time_sync_state = TimeSyncState(p.time_sync_state());
-      process.time_sync_mod_name = std::move(*p.mutable_time_sync_module_name());
-      process.component_init_info = std::move(*p.mutable_component_init_info());
-      process.ecal_runtime_version = std::move(*p.mutable_ecal_runtime_version());
+      process.process_id = p.process_id;
+      process.name = p.process_name;
+      process.host_name = p.host_name;
+      process.unit_name = p.unit_name;
+      process.params = p.process_parameter;
+      process.severity = Severity(p.state_severity);
+      process.severity_level = SeverityLevel(p.state_severity_level);
+      process.state_info = p.state_info;
+      process.time_sync_state = TimeSyncState(p.time_sync_state);
+      process.time_sync_mod_name = p.time_sync_module_name;
+      process.component_init_info = p.component_init_info;
+      process.ecal_runtime_version = p.ecal_runtime_version;
     }
 
-    for (auto &t : *mon_.mutable_topics())
+    auto process_topic_vector = [&](const std::vector<eCAL::Monitoring::STopic>& source_topics, Topic::Direction direction)
     {
-      auto &name = t.host_name();
-      auto &direction = t.direction();
-      auto found = hosts_map.find(name);
-      if(found != hosts_map.end())
+      for (const auto& t : source_topics)
       {
-        auto host = found->second;
-        if(direction == "publisher")
+        auto found = hosts_map.find(t.host_name);
+        if(found != hosts_map.end())
         {
-          host->publisher_count++;
-          host->data_sent_bytes += ((long long)t.topic_size() * (long long)t.data_frequency()) / 1000;
+          auto* host = found->second;
+          if(direction == Topic::Direction::PUBLISHER)
+          {
+            host->publisher_count++;
+            host->data_sent_bytes += ((long long)t.topic_size * (long long)t.data_frequency) / 1000;
+          }
+          else
+          {
+            host->subscriber_count++;
+            host->data_received_bytes += ((long long)t.topic_size * (long long)t.data_frequency) / 1000;
+          }
         }
-        else if(direction == "subscriber")
-        {
-          host->subscriber_count++;
-          host->data_received_bytes += ((long long)t.topic_size() * (long long)t.data_frequency()) / 1000;
-        }
-      }
-      auto &topic = topics.emplace_back();
-      topic.registration_clock = t.registration_clock();
-      topic.host_name = std::move(*t.mutable_host_name());
-      topic.process_id = t.process_id();
-      topic.process_name = std::move(*t.mutable_process_name());
-      topic.unit_name = std::move(*t.mutable_unit_name());
-      topic.id = std::move(*t.mutable_topic_id());
-      topic.name = std::move(*t.mutable_topic_name());
-      topic.direction = TopicDirection(t.direction());
-      topic.encoding = std::move(*t.mutable_datatype_information()->mutable_encoding());
-      topic.type = std::move(*t.mutable_datatype_information()->mutable_name());
-      topic.type_descriptor = std::move(*t.mutable_datatype_information()->mutable_descriptor_information());
-      for(auto &tl: t.transport_layer())
-      {
-        if (tl.active())
-        {
-          topic.transport_layers.emplace_back(TopicTransportLayer(tl.type()));
-        }
-      }
-      topic.size = t.topic_size();
-      topic.local_connections_count = t.connections_local();
-      topic.external_connections_count = t.connections_external();
-      topic.message_drops = t.message_drops();
-      topic.data_id = t.data_id();
-      topic.data_clock = t.data_clock();
-      topic.data_frequency_mhz = t.data_frequency();
-    }
 
-    for(auto &s: *mon_.mutable_services())
+        auto &topic = topics.emplace_back();
+        topic.registration_clock = t.registration_clock;
+        topic.host_name = t.host_name;
+        topic.process_id = t.process_id;
+        topic.process_name = t.process_name;
+        topic.unit_name = t.unit_name;
+        topic.id = std::to_string(t.topic_id);
+        topic.name = t.topic_name;
+        topic.direction = direction;
+        topic.encoding = t.datatype_information.encoding;
+        topic.type = t.datatype_information.name;
+        topic.type_descriptor = t.datatype_information.descriptor;
+        for(const auto &tl : t.transport_layer)
+        {
+          if (tl.active)
+          {
+            topic.transport_layers.emplace_back(TopicTransportLayer(tl.type));
+          }
+        }
+        topic.size = t.topic_size;
+        topic.local_connections_count = t.connections_local;
+        topic.external_connections_count = t.connections_external;
+        topic.message_drops = t.message_drops;
+        topic.data_id = t.data_id;
+        topic.data_clock = t.data_clock;
+        topic.data_frequency_mhz = t.data_frequency;
+      }
+    };
+
+    process_topic_vector(mon_.publishers, Topic::Direction::PUBLISHER);
+    process_topic_vector(mon_.subscribers, Topic::Direction::SUBSCRIBER);
+
+    for(const auto &s : mon_.servers)
     {
       auto &service = services.emplace_back();
-      service.id = s.service_id();
-      service.name = std::move(*s.mutable_service_name());
-      service.host_name = std::move(*s.mutable_host_name());
-      service.process_name = std::move(*s.mutable_process_name());
-      service.unit_name = std::move(*s.mutable_unit_name());
-      service.registration_clock = s.registration_clock();
-      service.tcp_port = s.tcp_port_v1();
-      for(auto &m: *s.mutable_methods())
+      service.id = std::to_string(s.service_id);
+      service.name = s.service_name;
+      service.host_name = s.host_name;
+      service.process_name = s.process_name;
+      service.unit_name = s.unit_name;
+      service.registration_clock = s.registration_clock;
+      service.tcp_port = static_cast<int>(s.tcp_port_v1);
+      for(const auto &m : s.methods)
       {
         auto &method = service.methods.emplace_back();
-        method.name = std::move(*m.mutable_method_name());
-        method.request_type = std::move(*m.mutable_req_type());
-        method.response_type = std::move(*m.mutable_resp_type());
-        method.call_count = m.call_count();
+        method.name = m.method_name;
+        method.request_type = m.request_datatype_information.name;
+        method.response_type = m.response_datatype_information.name;
+        method.call_count = m.call_count;
       }
     }
   }
@@ -250,9 +240,7 @@ class MonitorModel
 
   void Update()
   {
-    std::string raw_data;
-    eCAL::Monitoring::GetMonitoring(raw_data);
-    mon_.ParseFromString(raw_data);
+    eCAL::Monitoring::GetMonitoring(mon_);
     ProcessData();
     NotifyUpdate();
   }
